946e8280335cd2862f4431541847df8774582952
Reza Khan <rezakhan995@gmail.com>
2020-04-19 16:41:27 +0600

    date-time picker conditions applied accordint to trello board

--- core/options/posts/controls/date-picker/date-picker.php
+++ core/options/posts/controls/date-picker/date-picker.php
@@ -25,12 +25,12 @@ class DatePicker extends Structure {
      */
     public function load_scripts( $hook ) {
         wp_enqueue_script( 'date-picker', plugins_url( 'date-picker/assets/js/script.js', dirname( __FILE__ ) ) );
-        
-        $data['monday_first'] = (isset( $this->content['monday-first'] ) && ($this->content['monday-first'] == true) ) ? 1 : 0;
+
+        $data['monday_first'] = ( isset( $this->content['monday-first'] ) && ( $this->content['monday-first'] == true ) ) ? 1 : 0;
         $data['min_date'] = isset( $this->content['min-date'] ) ? $this->content['min-date'] : date( 'd-m-Y' );
         $data['max_date'] = isset( $this->content['max-date'] ) ? $this->content['max-date'] : '';
-                        
-        wp_localize_script('date-picker', 'date_picker_config', $data);
+
+        wp_localize_script( 'date-picker', 'date_picker_config', $data );
     }
 
     /**
@@ -60,7 +60,7 @@ class DatePicker extends Structure {
             <input type="text"
                     id="dm-date-picker"
                     name="<?php echo esc_html( $this->prefix . $name ); ?>"
-                    value="<?php echo esc_html( 'Y-m-d', $this->value ); ?>">
+                    value="<?php echo esc_html( date( 'Y-m-d', $this->value ) ); ?>">
         </div<>
     <?php
 }
---
+++ core/options/posts/controls/datetime-picker/assets/js/script.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function($) {
+
+    //Initialize the datepicker and set the first day of the week as Monday
+    if ($("#dm-datetime-picker").length) {
+
+        $('#dm-datetime-picker').datetimepicker({
+            timepicker: date_time_picker_config.timepicker,
+            datepicker: date_time_picker_config.datepicker,
+            dateFormat: date_time_picker_config.format,
+            minDate: date_time_picker_config.min_date,
+            maxDate: date_time_picker_config.max_date,
+            defaultTime: date_time_picker_config.default_time,
+            onSelect: function() {
+                $(this).val();
+            }
+        });
+    }
+});
