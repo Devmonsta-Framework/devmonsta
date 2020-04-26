6e6b94e453a3d36da90a032aa3dc7e07d345d636
Prappo <prappo@xpeedstudio.com>
2020-04-26 14:36:29 +0600

    Update text.php

--- core/options/taxonomies/controls/text/text.php
+++ core/options/taxonomies/controls/text/text.php
@@ -42,10 +42,18 @@ class Text extends Structure
 
     public function columns()
     {
+        $visible = true;
         $content = $this->content;
-        add_filter('manage_edit-' . $this->taxonomy . '_columns', function ($columns) use ($content) {
+        add_filter('manage_edit-' . $this->taxonomy . '_columns', function ($columns) use ($content,$visible) {
 
-            $columns[$content['name']] = __($content['label'], 'devmonsta');
+            if (isset($content['show_in_table'])) {
+                if ($content['show_in_table'] == false) {
+                    $visible = false;
+                }
+            }
+            if ($visible) {
+                $columns[$content['name']] = __($content['label'], 'devmonsta');
+            }
 
             return $columns;
         });
@@ -68,12 +76,12 @@ class Text extends Structure
 
         $prefix = 'devmonsta_';
         $name = $prefix . $this->content['name'];
-        $value = get_term_meta($term->term_id,$name,true);
+        $value = get_term_meta($term->term_id, $name, true);
         ?>
 
         <tr class="form-field term-group-wrap">
             <th scope="row"><label for="feature-group"><?php echo esc_html($this->content['label']); ?></label></th>
-            <td> <input name="<?php echo $name; ?>" id="<?php echo $name; ?>" type="text" value="<?php echo $value;?>" size="40" aria-required="true"></td>
+            <td> <input name="<?php echo $name; ?>" id="<?php echo $name; ?>" type="text" value="<?php echo $value; ?>" size="40" aria-required="true"></td>
         </tr>
 
 
