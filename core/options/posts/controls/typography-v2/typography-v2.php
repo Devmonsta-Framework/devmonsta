6909023b72a141ff00f3f862073621f3121d7dc5
nitu-xs <nitu@xpeedstudio.com>
2020-04-30 09:25:51 +0600

    typography working

--- core/options/posts/controls/typography-v2/typography-v2.php
+++ core/options/posts/controls/typography-v2/typography-v2.php
@@ -14,7 +14,33 @@ class TypographyV2 extends Structure {
     {
 
     }
-
+		/**
+		 * Return the list of Google Fonts from our json file. Unless otherwise specfied, list will be limited to 30 fonts.
+		 */
+		public function customizer_getGoogleFonts( $count = 30 ) {
+            $transient = "_newseqo_customizer_google_fonts";
+            $key = 'AIzaSyAP_KwRh1HX7tNAbLDSGhZ8K4tfu4MW4kA';
+            if(get_transient($transient)==false){
+               $google_api= 'https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key='.$key.'';
+                  
+               $request = wp_remote_get( DM_OPTIONS.'/posts/controls/typography-v2/google-fonts-popularity.json' );
+              
+               if( is_wp_error( $request ) ) {
+                  return "";
+               }
+               $body = wp_remote_retrieve_body( $request );
+               $content = json_decode( $body );
+               set_transient( $transient, $content, 86000 );
+            }else{
+                $content =get_transient($transient); 
+            }
+            
+               if( $count == 'all' ) {
+                   return $content->items;
+               } else {
+                   return array_slice( $content->items, 0, $count );
+            }
+           }
     /**
      * @internal
      */
@@ -33,7 +59,7 @@ class TypographyV2 extends Structure {
     public function load_scripts($hook)
     {
         // js 
-        wp_enqueue_script( 'dm-select2', DM_OPTIONS . 'options/posts/controls/typography-v2/assets/js/select2.min.js', ['jquery'], true );
+        wp_enqueue_script( 'dm-select2', DM_OPTIONS . 'posts/controls/typography-v2/assets/js/select2.min.js', ['jquery'], true );
         //css
         wp_enqueue_style( 'dm-slide-ranger-css', plugins_url( 'typography-v2/assets/css/ranger-slider.css', dirname( __FILE__ ) ) );
         wp_enqueue_style( 'dm-select2', plugins_url( 'typography-v2/assets/css/select2.min.css', dirname( __FILE__ ) ) );
@@ -89,12 +115,13 @@ class TypographyV2 extends Structure {
         }
 
         wp_enqueue_script( 'dm-typo-script-handle', DM_CORE . 'options/posts/controls/typography-v2/assets/js/scripts.js', ['jquery', 'wp-color-picker'], false, true );
-
+        $google_fonts = $this->dm_getGoogleFonts();
         global $post;
         $data            = [];
         $data['default'] = ( !is_null( get_post_meta( $post->ID, $this->prefix . 'typograhy_color' , true ) ) ) 
                             ? get_post_meta( $post->ID, $this->prefix . 'typograhy_color' , true )
                             : $this->content['value']['color'];
+        $data['google_fonts'] =  $google_fonts;                  
         wp_localize_script( 'dm-script-handle', 'color_picker_config', $data );
     }
 
@@ -112,7 +139,25 @@ class TypographyV2 extends Structure {
         : $content['value']['color'];
         $typo_graphy['color'] == '' ? $typo_graphy['color'] = $content['value']['color'] : $typo_graphy['color'];
 
-        // font size 
+        // font family 
+        $typo_graphy['family'] = !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_family", true ) ) ?
+        get_post_meta( $post->ID, $this->prefix . "typograhy_family", true )
+        : $content['value']['family'];
+        $typo_graphy['family'] == '' ? $typo_graphy['family'] = $content['value']['family'] : $typo_graphy['family'];
+
+        // font style 
+        $typo_graphy['style'] = !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_style", true ) ) ?
+        get_post_meta( $post->ID, $this->prefix . "typograhy_style", true )
+        : $content['value']['style'];
+        $typo_graphy['style'] == '' ? $typo_graphy['style'] = $content['value']['style'] : $typo_graphy['style'];
+
+        // font weight 
+        $typo_graphy['weight'] = !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_weight", true ) ) ?
+        get_post_meta( $post->ID, $this->prefix . "typograhy_weight", true )
+        : $content['value']['weight'];
+        $typo_graphy['weight'] == '' ? $typo_graphy['weight'] = $content['value']['weight'] : $typo_graphy['weight'];
+
+         // font size 
         $typo_graphy['size'] = !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_size", true ) ) ?
         get_post_meta( $post->ID, $this->prefix . "typograhy_size", true )
         : $content['value']['size'];
@@ -140,7 +185,6 @@ class TypographyV2 extends Structure {
     public function output() {
         $font_list     = $this->dm_getGoogleFonts();
         $label        = isset( $this->content['label'] ) ? $this->content['label'] : '';
-        $name         = isset( $this->content['name'] ) ? $this->content['name'] : '';
         $desc         = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
         $value        = isset( $this->content['value'] ) ? $this->content['value'] : [];
         $components   = isset( $this->content['components'] ) ? $this->content['components'] : [];
@@ -170,9 +214,15 @@ class TypographyV2 extends Structure {
                                     <select class="google-fonts-list" name="<?php echo esc_attr($this->prefix . "typograhy_family") ?>">
                                         <?php
                                             foreach( $font_list as $key => $item ) {
-                                                    echo '<option value="' . $key . '" '  . '>' . $item . '</option>';
+                                                    $selected = $key == esc_html($value['family']) ? 'selected' : '';
+                                                    echo '<option value="' . $key . '" '. $selected . '>' . $item . '</option>';
                                                 }
                                         ?>
+                                    </select>
+                                </div>
+                                <div class="style-weight">
+                                    <select class="google-styel-list">
+
                                     </select>
                                 </div>
                             </div>
@@ -183,21 +233,21 @@ class TypographyV2 extends Structure {
                         echo "<div>".esc_html( 'Size :')."</div>";
                         ?>
                         <input class="range-slider-font-size dm_group_typhography_range_font_size" type="range" min="0" max="100"   value="<?php echo (float)$value['size']; ?>">
-                        <input type="text" name="<?php echo esc_attr($this->prefix . "typograhy_size") ?>" value=" <?php echo esc_html(trim( $value['size'] )) ; ?> " class="google-fonts-size-style" id="size_value" /> 
+                        <input type="text" name="<?php echo esc_attr($this->prefix . "typograhy_size") ?>" value=" <?php echo esc_html(trim( $value['size'] )) ; ?> "  id="size_value" /> 
                         <?php
                         break;
                     case 'line-height':
                         echo "<div>".esc_html( 'Line height:')."</div>";
                         ?>                        
                         <input class="range-slider-line-height dm_group_typhography_line_height" type="range" min="0" max="100"  value="<?php echo (float)$this->value["line-height"]; ?>">
-                        <input type="text" name="<?php echo esc_attr($this->prefix . "typograhy_line_height") ?>" value=" <?php echo esc_html(trim($value["line-height"])); ?> " class="google-fonts-lineheight-style custom_font_line_height" id="line_height_value" /> 
+                        <input type="text" name="<?php echo esc_attr($this->prefix . "typograhy_line_height") ?>" value=" <?php echo esc_html(trim($value["line-height"])); ?> "  id="line_height_value" /> 
                         <?php
                         break;
                     case 'letter-spacing':
                         echo "<div>".esc_html( 'Later space:')."</div>";
                         ?>
-                           <input class="range-slide-letter-space dm_group_typhography_letterspace" type="range" min="0" max="100"  value="<?php echo (float)$this->value["letter-spacing"]; ?>">
-                           <input type="text" name="<?php echo esc_attr($this->prefix . "typograhy_letter_spacing") ?>" value="<?php echo esc_html(trim( $value["letter-spacing"]) ); ?>" class="google-fonts-letterspace" id="latter_spacing_value" /> 
+                           <input class="range-slide-letter-space dm_group_typhography_letterspace" type="range" min="-10" max="10"  value="<?php echo (float)$this->value["letter-spacing"]; ?>">
+                           <input type="text" name="<?php echo esc_attr($this->prefix . "typograhy_letter_spacing") ?>" value="<?php echo esc_html(trim( $value["letter-spacing"]) ); ?>" id="latter_spacing_value" /> 
                         <?php
                         break;
                     case 'color':
