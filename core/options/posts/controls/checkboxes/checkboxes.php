b4162bd9c09cb9f4da4a689ca38d915df3520894
Reza Khan <rezakhan995@gmail.com>
2020-05-06 11:40:30 +0600

    multiple checkbox control made dynamic to be used from both post meta and taxonomy meta

--- core/options/posts/controls/checkboxes/checkboxes.php
+++ core/options/posts/controls/checkboxes/checkboxes.php
@@ -6,6 +6,8 @@ use Devmonsta\Options\Posts\Structure;
 
 class Checkboxes extends Structure {
 
+    protected $current_screen;
+
     /**
      * @internal
      */
@@ -24,26 +26,33 @@ class Checkboxes extends Structure {
      * @internal
      */
     public function render() {
-        global $post;
-        $content             = $this->content;
-        $default_value_array = [];
+        global $wpdocs_admin_page;
+        $screen               = get_current_screen();
+        $this->current_screen = $screen->base;
 
-        if ( is_array( $content['value'] ) && !empty( $content['value'] ) ) {
+        if ( $this->current_screen == "post" ) {
+            global $post;
+            $content             = $this->content;
+            $default_value_array = [];
 
-            foreach ( $content['value'] as $default_key => $default_value ) {
+            if ( is_array( $content['value'] ) && !empty( $content['value'] ) ) {
 
-                if ( $default_value == true ) {
-                    array_push( $default_value_array, $default_key );
+                foreach ( $content['value'] as $default_key => $default_value ) {
+
+                    if ( $default_value == true ) {
+                        array_push( $default_value_array, $default_key );
+                    }
+
                 }
 
             }
 
+            $this->value = ( !empty( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) )
+                && !is_null( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) ) )
+            ? maybe_unserialize( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) )
+            : $default_value_array;
         }
 
-        $this->value = ( !empty( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) )
-                        && !is_null( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) ) )
-                            ? maybe_unserialize( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) )
-                            : $default_value_array;
         $this->output();
     }
 
@@ -52,56 +61,154 @@ class Checkboxes extends Structure {
      */
     public function output() {
         $label   = isset( $this->content['label'] ) ? $this->content['label'] : '';
-        $name    = isset( $this->content['name'] ) ? $this->content['name'] : '';
+        $prefix  = 'devmonsta_';
+        $name    = isset( $this->content['name'] ) ? $prefix . $this->content['name'] : '';
         $desc    = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
         $attrs   = isset( $this->content['attr'] ) ? $this->content['attr'] : '';
         $choices = isset( $this->content['choices'] ) ? $this->content['choices'] : '';
 
         $default_attributes = "";
-        $dynamic_classes = "";
+        $dynamic_classes    = "";
+
         if ( is_array( $attrs ) && !empty( $attrs ) ) {
 
             foreach ( $attrs as $key => $val ) {
-                if($key == "class"){
+
+                if ( $key == "class" ) {
                     $dynamic_classes .= $val . " ";
-                }else{
+                } else {
                     $default_attributes .= $key . "='" . $val . "' ";
                 }
-               
+
             }
 
         }
+
         $class_attributes = "class='dm-option $dynamic_classes'";
         $default_attributes .= $class_attributes;
 
         ?>
-        <div <?php echo dm_render_markup($default_attributes);?> >
+        <div <?php echo dm_render_markup( $default_attributes ); ?> >
                 <label><?php echo esc_html( $label ); ?> </label>
                 <div><small><?php echo esc_html( $desc ); ?> </small></div>
         <?php
 
         foreach ( $choices as $id => $element ) {
 
-            if ( is_array( $this->value ) && in_array( $id, $this->value ) ) {
+            if ( $this->current_screen == "post" && is_array( $this->value ) && in_array( $id, $this->value ) ) {
                 $checked = 'checked="checked"';
             } else {
                 $checked = null;
             }
 
             ?>
-                    <input  type="checkbox"
-                    name="<?php echo esc_attr( $this->prefix . $name ); ?>[]"
-                     value="<?php echo esc_attr($id); ?>" <?php echo esc_attr($checked); ?> />
-                    <?php echo esc_html($element); ?>
+                <input  type="checkbox"
+                        id="<?php echo $name; ?>"
+                        name="<?php echo esc_attr( $name ); ?>[]"
+                        value="<?php echo esc_attr( $id ); ?>" <?php echo esc_attr( $checked ); ?> />
+                    <?php echo esc_html( $element ); ?>
             <?php
 }
 
         ?>
-        <input type="text" value="default" name="<?php echo esc_attr( $this->prefix . $name ); ?>[]" style="display: none">
-
+        <input type="text" value="default" name="<?php echo esc_attr( $name ); ?>[]" style="display: none">
 </div>
 <?php
 
     }
+
+    public function columns() {
+        $visible = false;
+        $content = $this->content;
+        add_filter( 'manage_edit-' . $this->taxonomy . '_columns',
+            function ( $columns ) use ( $content, $visible ) {
+
+                $visible = ( isset( $content['show_in_table'] ) && $content['show_in_table'] === true ) ? true : false;
+
+                if ( $visible ) {
+                    $columns[$content['name']] = __( $content['label'], 'devmonsta' );
+                }
+
+                return $columns;
+            } );
+
+        $cc = $content;
+        add_filter( 'manage_' . $this->taxonomy . '_custom_column',
+            function ( $content, $column_name, $term_id ) use ( $cc ) {
+
+                if ( $column_name == $cc['name'] ) {
+                    echo esc_html(  ( maybe_unserialize( $term_id, 'devmonsta_' . $column_name, true ) == true ) ? "yes" : "no" );
+                }
+
+                return $content;
+
+            }, 10, 3 );
+
+    }
+
+    public function edit_fields( $term, $taxonomy ) {
+        $prefix  = 'devmonsta_';
+        $label   = isset( $this->content['label'] ) ? $this->content['label'] : '';
+        $name    = isset( $this->content['name'] ) ? $prefix . $this->content['name'] : '';
+        $desc    = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
+        $attrs   = isset( $this->content['attr'] ) ? $this->content['attr'] : '';
+        $value   = maybe_unserialize( get_term_meta( $term->term_id, $name, true ) );
+        $choices = isset( $this->content['choices'] ) ? $this->content['choices'] : '';
+
+        $default_attributes = "";
+        $dynamic_classes    = "";
+
+        if ( is_array( $attrs ) && !empty( $attrs ) ) {
+
+            foreach ( $attrs as $key => $val ) {
+
+                if ( $key == "class" ) {
+                    $dynamic_classes .= $val . " ";
+                } else {
+                    $default_attributes .= $key . "='" . $val . "' ";
+                }
+
+            }
+
+        }
+
+        $class_attributes = "class='dm-option term-group-wrap $dynamic_classes'";
+        $default_attributes .= $class_attributes;
+
+        ?>
+
+        <tr <?php echo dm_render_markup( $default_attributes ); ?> >
+            <th scope="row">
+                <label for="feature-group"><?php echo esc_html( $label ); ?></label>
+            </th>
+            <td>
+            <?php
+
+        foreach ( $choices as $id => $element ) {
+
+            if ( is_array( $value ) && in_array( $id, $value ) ) {
+                $checked = 'checked="checked"';
+            } else {
+                $checked = null;
+            }
+
+            ?>
+                <input  type="checkbox"
+                        id="<?php echo $name; ?>"
+                        name="<?php echo esc_attr( $name ); ?>[]"
+                        value="<?php echo esc_attr( $id ); ?>" <?php echo esc_attr( $checked ); ?> />
+                    <?php echo esc_html( $element ); ?>
+            <?php
+}
+
+        ?>
+                <input type="text" value="default" name="<?php echo esc_attr( $name ); ?>[]" style="display: none">
+
+
+                <br>(<?php echo esc_html( $desc ); ?> )
+            </td>
+        </tr>
+    <?php
+}
 
 }
