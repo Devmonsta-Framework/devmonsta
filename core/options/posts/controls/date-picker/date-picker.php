9b24a7a4477fd915d1758d271decfdce8cf6b7ec
Reza Khan <rezakhan995@gmail.com>
2020-05-07 15:56:46 +0600

    html and date-picker updated to be used from single control file

--- core/options/posts/controls/date-picker/date-picker.php
+++ core/options/posts/controls/date-picker/date-picker.php
@@ -16,8 +16,8 @@ class DatePicker extends Structure {
     /**
      * @internal
      */
-    public function enqueue() {
-
+    public function enqueue( $meta_owner ) {
+        $this->current_screen = $meta_owner;
     }
 
     /**
@@ -26,8 +26,10 @@ class DatePicker extends Structure {
     public function render() {
         $content = $this->content;
         global $post;
-        $this->value = !is_null( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) ) ?
-        get_post_meta( $post->ID, $this->prefix . $content['name'], true )
+        $this->value = (  ( $this->current_screen == "post" )
+            && ( !is_null( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) ) )
+            && ( "" != get_post_meta( $post->ID, $this->prefix . $content['name'], true ) ) )
+        ? get_post_meta( $post->ID, $this->prefix . $content['name'], true )
         : $content['value'];
         $this->output();
     }
@@ -37,7 +39,8 @@ class DatePicker extends Structure {
      */
     public function output() {
         $label              = isset( $this->content['label'] ) ? $this->content['label'] : '';
-        $name               = isset( $this->content['name'] ) ? $this->content['name'] : '';
+        $prefix             = 'devmonsta_';
+        $name               = isset( $this->content['name'] ) ? $prefix . $this->content['name'] : '';
         $desc               = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
         $attrs              = isset( $this->content['attr'] ) ? $this->content['attr'] : '';
         $min_date           = isset( $this->content['min-date'] ) ? $this->content['min-date'] : date( 'd-m-Y' );
@@ -66,11 +69,85 @@ class DatePicker extends Structure {
         <div <?php echo dm_render_markup( $default_attributes ); ?> >
             <label class="dm-option-label"><?php echo esc_html( $label ); ?> </label>
             <div><small class="dm-option-desc"><?php echo esc_html( $desc ); ?> </small></div>
-            <input type="date" name="<?php echo esc_attr( $this->prefix . $name ); ?>"
+            <input type="date" name="<?php echo esc_attr( $name ); ?>"
                     value="<?php echo esc_attr( $this->value ); ?>"
                     min="<?php echo esc_attr( $min_date ) ?>" max="<?php echo esc_attr( $max_date ) ?>">
-        </div<>
+        </div>
     <?php
+}
+
+    public function columns() {
+        $visible = false;
+        $content = $this->content;
+        add_filter( 'manage_edit-' . $this->taxonomy . '_columns',
+            function ( $columns ) use ( $content, $visible ) {
+
+                $visible = ( isset( $content['show_in_table'] ) && $content['show_in_table'] === true ) ? true : false;
+
+                if ( $visible ) {
+                    $columns[$content['name']] = __( $content['label'], 'devmonsta' );
+                }
+
+                return $columns;
+            } );
+
+        $cc = $content;
+        add_filter( 'manage_' . $this->taxonomy . '_custom_column',
+            function ( $content, $column_name, $term_id ) use ( $cc ) {
+
+                if ( $column_name == $cc['name'] ) {
+                    echo esc_html( get_term_meta( $term_id, 'devmonsta_' . $column_name, true ) );
+                }
+
+                return $content;
+
+            }, 10, 3 );
+
+    }
+
+    public function edit_fields( $term, $taxonomy ) {
+        $label              = isset( $this->content['label'] ) ? $this->content['label'] : '';
+        $prefix             = 'devmonsta_';
+        $name               = isset( $this->content['name'] ) ? $prefix . $this->content['name'] : '';
+        $desc               = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
+        $attrs              = isset( $this->content['attr'] ) ? $this->content['attr'] : '';
+        $value              = get_term_meta( $term->term_id, $name, true );
+        $min_date           = isset( $this->content['min-date'] ) ? $this->content['min-date'] : date( 'd-m-Y' );
+        $max_date           = isset( $this->content['max-date'] ) ? $this->content['max-date'] : '';
+        $default_attributes = "";
+        $dynamic_classes    = "";
+
+        if ( is_array( $attrs ) && !empty( $attrs ) ) {
+
+            foreach ( $attrs as $key => $val ) {
+
+                if ( $key == "class" ) {
+                    $dynamic_classes .= $val . " ";
+                } else {
+                    $default_attributes .= $key . "='" . $val . "' ";
+                }
+
+            }
+
+        }
+
+        $class_attributes = "class='dm-option term-group-wrap $dynamic_classes'";
+        $default_attributes .= $class_attributes;
+
+        ?>
+
+    <tr <?php echo dm_render_markup( $default_attributes ); ?> >
+        <th scope="row">
+            <label class="dm-option-label"><?php echo esc_html( $label ); ?></label>
+        </th>
+        <td>
+            <input type="date" name="<?php echo esc_attr( $name ); ?>"
+                    value="<?php echo esc_attr( $value ); ?>"
+                    min="<?php echo esc_attr( $min_date ) ?>" max="<?php echo esc_attr( $max_date ) ?>">
+            <br><small class="dm-option-desc">(<?php echo esc_html( $desc ); ?> )</small>
+        </td>
+    </tr>
+<?php
 }
 
 }
