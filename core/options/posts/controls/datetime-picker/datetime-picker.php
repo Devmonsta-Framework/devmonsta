6aaa2422920778c50304fab4233271ff02807f17
Reza Khan <rezakhan995@gmail.com>
2020-05-10 14:29:39 +0600

    date-time and date-time-range updated

--- core/options/posts/controls/datetime-picker/datetime-picker.php
+++ core/options/posts/controls/datetime-picker/datetime-picker.php
@@ -6,6 +6,8 @@ use Devmonsta\Options\Posts\Structure;
 
 class DatetimePicker extends Structure {
 
+    protected $current_screen;
+
     /**
      * @internal
      */
@@ -16,10 +18,24 @@ class DatetimePicker extends Structure {
     /**
      * @internal
      */
-    public function enqueue() {
+
+    public function enqueue( $meta_owner ) {
+        $this->current_screen = $meta_owner;
+
+        if ( $this->current_screen == "post" ) {
+
+            $this->enqueue_date_time_picker_scripts();
+        } elseif ( $this->current_screen == "taxonomy" ) {
+            add_action( 'init', [$this, 'enqueue_date_time_picker_scripts'] );
+
+        }
+
+    }
+
+    public function enqueue_date_time_picker_scripts() {
         wp_enqueue_style( 'date-time-picker', DM_CORE . 'options/posts/controls/datetime-picker/assets/css/jquery.datetimepicker.min.css' );
-        wp_enqueue_script( 'date-time-picker', DM_CORE . 'options/posts/controls/datetime-picker/assets/js/jquery.datetimepicker.full.min.js', [ 'jquery' ] );
-        wp_enqueue_script( 'dm-date-time-picker', DM_CORE . 'options/posts/controls/datetime-picker/assets/js/script.js', [ 'jquery', 'date-time-picker' ] );
+        wp_enqueue_script( 'date-time-picker', DM_CORE . 'options/posts/controls/datetime-picker/assets/js/jquery.datetimepicker.full.min.js', ['jquery'] );
+        wp_enqueue_script( 'dm-date-time-picker', DM_CORE . 'options/posts/controls/datetime-picker/assets/js/script.js', ['jquery', 'date-time-picker'] );
 
         $date_time_picker_config               = $this->content['datetime-picker'];
         $date_time_picker_data['format']       = isset( $date_time_picker_config['format'] ) ? $date_time_picker_config['format'] : 'Y-m-d H:i';
@@ -30,17 +46,20 @@ class DatetimePicker extends Structure {
         $date_time_picker_data['default_time'] = isset( $date_time_picker_config['defaultTime'] ) ? $date_time_picker_config['defaultTime'] : '12:00';
 
         wp_localize_script( 'dm-date-time-picker', 'date_time_picker_config', $date_time_picker_data );
+
     }
 
     /**
      * @internal
      */
     public function render() {
-        $content = $this->content;
+        $content           = $this->content;
+        $default_date_time = isset( $content['value'] ) ? $content['value'] : "";
         global $post;
-        $this->value = !is_null( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) ) ?
+        $this->value = (  ( $this->current_screen == "post" )
+            && !is_null( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) ) ) ?
         get_post_meta( $post->ID, $this->prefix . $content['name'], true )
-        : $content['value'];
+        : $default_date_time;
         $this->output();
     }
 
@@ -49,7 +68,8 @@ class DatetimePicker extends Structure {
      */
     public function output() {
         $label              = isset( $this->content['label'] ) ? $this->content['label'] : '';
-        $name               = isset( $this->content['name'] ) ? $this->content['name'] : '';
+        $prefix             = 'devmonsta_';
+        $name               = isset( $this->content['name'] ) ? $prefix . $this->content['name'] : '';
         $desc               = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
         $attrs              = isset( $this->content['attr'] ) ? $this->content['attr'] : '';
         $default_attributes = "";
@@ -78,10 +98,87 @@ class DatetimePicker extends Structure {
             <div><small class="dm-option-desc"><?php echo esc_html( $desc ); ?> </small></div>
             <input type="text"
                     id="dm-datetime-picker"
-                    name="<?php echo esc_attr( $this->prefix . $name ); ?>"
+                    name="<?php echo esc_attr( $name ); ?>"
                     value="<?php echo esc_attr( $this->value ); ?>">
-        </div<>
+        </div>
     <?php
+}
+
+    public function columns() {
+        $visible = false;
+        $content = $this->content;
+        add_filter( 'manage_edit-' . $this->taxonomy . '_columns',
+            function ( $columns ) use ( $content, $visible ) {
+
+                $visible = ( isset( $content['show_in_table'] ) && $content['show_in_table'] === true ) ? true : false;
+
+                if ( $visible ) {
+                    $columns[$content['name']] = __( $content['label'], 'devmonsta' );
+                }
+
+                return $columns;
+            } );
+
+        $cc = $content;
+        add_filter( 'manage_' . $this->taxonomy . '_custom_column',
+            function ( $content, $column_name, $term_id ) use ( $cc ) {
+
+                if ( $column_name == $cc['name'] ) {
+                    echo esc_html( get_term_meta( $term_id, 'devmonsta_' . $column_name, true ) );
+                }
+
+                return $content;
+
+            }, 10, 3 );
+
+    }
+
+    public function edit_fields( $term, $taxonomy ) {
+
+        $this->enqueue_date_time_picker_scripts();
+        $label              = isset( $this->content['label'] ) ? $this->content['label'] : '';
+        $prefix             = 'devmonsta_';
+        $name               = isset( $this->content['name'] ) ? $prefix . $this->content['name'] : '';
+        $desc               = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
+        $attrs              = isset( $this->content['attr'] ) ? $this->content['attr'] : '';
+        $value              = get_term_meta( $term->term_id, $name, true );
+        $min_date           = isset( $this->content['min-date'] ) ? $this->content['min-date'] : date( 'd-m-Y' );
+        $max_date           = isset( $this->content['max-date'] ) ? $this->content['max-date'] : '';
+        $default_attributes = "";
+        $dynamic_classes    = "";
+
+        if ( is_array( $attrs ) && !empty( $attrs ) ) {
+
+            foreach ( $attrs as $key => $val ) {
+
+                if ( $key == "class" ) {
+                    $dynamic_classes .= $val . " ";
+                } else {
+                    $default_attributes .= $key . "='" . $val . "' ";
+                }
+
+            }
+
+        }
+
+        $class_attributes = "class='dm-option term-group-wrap $dynamic_classes'";
+        $default_attributes .= $class_attributes;
+
+        ?>
+
+<tr <?php echo dm_render_markup( $default_attributes ); ?> >
+    <th scope="row">
+        <label class="dm-option-label"><?php echo esc_html( $label ); ?></label>
+    </th>
+    <td>
+        <input type="text"
+                    id="dm-datetime-picker"
+                    name="<?php echo esc_attr( $name ); ?>"
+                    value="<?php echo esc_attr( $value ); ?>">
+        <br><small class="dm-option-desc">(<?php echo esc_html( $desc ); ?> )</small>
+    </td>
+</tr>
+<?php
 }
 
 }
--- core/options/posts/controls/datetime-range/datetime-range.php
+++ core/options/posts/controls/datetime-range/datetime-range.php
@@ -6,6 +6,8 @@ use Devmonsta\Options\Posts\Structure;
 
 class DatetimeRange extends Structure {
 
+    protected $current_screen;
+
     /**
      * @internal
      */
@@ -16,11 +18,24 @@ class DatetimeRange extends Structure {
     /**
      * @internal
      */
-    public function enqueue() {
+    public function enqueue( $meta_owner ) {
+        $this->current_screen = $meta_owner;
+
+        if ( $this->current_screen == "post" ) {
+
+            $this->enqueue_date_time_range_scripts();
+        } elseif ( $this->current_screen == "taxonomy" ) {
+            add_action( 'init', [$this, 'enqueue_date_time_range_scripts'] );
+        }
+
+    }
+
+    public function enqueue_date_time_range_scripts() {
+
         wp_enqueue_style( 'date-time-range', DM_CORE . 'options/posts/controls/datetime-range/assets/css/daterangepicker.css' );
-        wp_enqueue_script( 'date-time-range', DM_CORE . 'options/posts/controls/datetime-range/assets/js/daterangepicker.js', ['jquery'] );
+        wp_enqueue_script( 'date-time-range', DM_CORE . 'options/posts/controls/datetime-range/assets/js/daterangepicker.js', ['jquery', 'date-time-range-moment'] );
         wp_enqueue_script( 'date-time-range-moment', DM_CORE . 'options/posts/controls/datetime-range/assets/js/moment.min.js', ['jquery'] );
-        wp_enqueue_script( 'dm-date-time-range', DM_CORE . 'options/posts/controls/datetime-range/assets/js/script.js', ['jquery', 'date-time-range'] );
+        wp_enqueue_script( 'dm-date-time-range', DM_CORE . 'options/posts/controls/datetime-range/assets/js/script.js', ['jquery', 'date-time-range-moment','date-time-range'] );
 
         $date_time_range_config = $this->content['datetime-pickers'];
         $data['min_date']       = isset( $date_time_range_config['minDate'] ) ? date( "YYYY-MM-DD", strtotime( $date_time_range_config['minDate'] ) ) : date( "YYYY-MM-DD" );
@@ -30,17 +45,23 @@ class DatetimeRange extends Structure {
         $data['timepicker']     = ( $date_time_range_config['timepicker'] ) ? "true" : "";
         $data['time24hours']    = ( $date_time_range_config['time24hours'] ) ? "true" : "";
         wp_localize_script( 'dm-date-time-range', 'date_time_range_config', $data );
+
     }
 
     /**
      * @internal
      */
     public function render() {
-        $content = $this->content;
+        $content      = $this->content;
+        $default_time = ( isset( $content['value']['from'] ) && isset( $content['value']['to'] ) ) ? ( date( "Y-m-d h:m a", strtotime( $content['value']['from'] ) ) . " - " . date( "Y-m-d h:m a", strtotime( $content['value']['to'] ) ) ) : "";
         global $post;
-        $this->value = !is_null( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) ) ?
-        get_post_meta( $post->ID, $this->prefix . $content['name'], true )
-        : $content['value'];
+
+        // var_dump($default_time);
+        $this->value = (  ( $this->current_screen == "post" )
+            && ( !is_null( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) ) )
+            && ( "" != get_post_meta( $post->ID, $this->prefix . $content['name'], true ) ) )
+        ? get_post_meta( $post->ID, $this->prefix . $content['name'], true )
+        : $default_time;
         $this->output();
     }
 
@@ -49,7 +70,8 @@ class DatetimeRange extends Structure {
      */
     public function output() {
         $label              = isset( $this->content['label'] ) ? $this->content['label'] : '';
-        $name               = isset( $this->content['name'] ) ? $this->content['name'] : '';
+        $prefix             = 'devmonsta_';
+        $name               = isset( $this->content['name'] ) ? $prefix . $this->content['name'] : '';
         $desc               = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
         $attrs              = isset( $this->content['attr'] ) ? $this->content['attr'] : '';
         $default_attributes = "";
@@ -78,10 +100,84 @@ class DatetimeRange extends Structure {
             <div><small class="dm-option-desc"><?php echo esc_html( $desc ); ?> </small></div>
             <input type="text"
                     id="dm-datetime-range"
-                    name="<?php echo esc_attr( $this->prefix . $name ); ?>"
+                    name="<?php echo esc_attr( $name ); ?>"
                     value="<?php echo esc_attr( $this->value ); ?>">
-        </div<>
+        </div>
     <?php
+}
+
+    public function columns() {
+        $visible = false;
+        $content = $this->content;
+        add_filter( 'manage_edit-' . $this->taxonomy . '_columns',
+            function ( $columns ) use ( $content, $visible ) {
+
+                $visible = ( isset( $content['show_in_table'] ) && $content['show_in_table'] === true ) ? true : false;
+
+                if ( $visible ) {
+                    $columns[$content['name']] = __( $content['label'], 'devmonsta' );
+                }
+
+                return $columns;
+            } );
+
+        $cc = $content;
+        add_filter( 'manage_' . $this->taxonomy . '_custom_column',
+            function ( $content, $column_name, $term_id ) use ( $cc ) {
+
+                if ( $column_name == $cc['name'] ) {
+                    echo esc_html( get_term_meta( $term_id, 'devmonsta_' . $column_name, true ) );
+                }
+
+                return $content;
+
+            }, 10, 3 );
+
+    }
+
+    public function edit_fields( $term, $taxonomy ) {
+        $this->enqueue_date_time_range_scripts();
+        $label              = isset( $this->content['label'] ) ? $this->content['label'] : '';
+        $prefix             = 'devmonsta_';
+        $name               = isset( $this->content['name'] ) ? $prefix . $this->content['name'] : '';
+        $desc               = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
+        $attrs              = isset( $this->content['attr'] ) ? $this->content['attr'] : '';
+        $value              = get_term_meta( $term->term_id, $name, true );
+        $default_attributes = "";
+        $dynamic_classes    = "";
+
+        if ( is_array( $attrs ) && !empty( $attrs ) ) {
+
+            foreach ( $attrs as $key => $val ) {
+
+                if ( $key == "class" ) {
+                    $dynamic_classes .= $val . " ";
+                } else {
+                    $default_attributes .= $key . "='" . $val . "' ";
+                }
+
+            }
+
+        }
+
+        $class_attributes = "class='dm-option term-group-wrap $dynamic_classes'";
+        $default_attributes .= $class_attributes;
+
+        ?>
+
+<tr <?php echo dm_render_markup( $default_attributes ); ?> >
+    <th scope="row">
+        <label class="dm-option-label"><?php echo esc_html( $label ); ?></label>
+    </th>
+    <td>
+        <input type="text"
+                    id="dm-datetime-range"
+                    name="<?php echo esc_attr( $name ); ?>"
+                    value="<?php echo esc_attr( $value ); ?>">
+        <br><small class="dm-option-desc">(<?php echo esc_html( $desc ); ?> )</small>
+    </td>
+</tr>
+<?php
 }
 
 }
