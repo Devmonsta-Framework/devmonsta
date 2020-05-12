ddeeaaa56bbe77dc4f45b08769de01efd4c51bcf
Reza Khan <rezakhan995@gmail.com>
2020-05-12 13:31:43 +0600

    typography updated and tested, working fine

--- core/options/posts/controls/typography/typography.php
+++ core/options/posts/controls/typography/typography.php
@@ -60,81 +60,7 @@ class Typography extends Structure {
      */
     public function render() {
         $content = $this->content;
-
-        $typo_graphy = [];
         global $post;
-
-// // color
-
-// $typo_graphy['color'] = ( $this->current_screen == "post" ) && !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_color", true ) ) ?
-
-// get_post_meta( $post->ID, $this->prefix . "typograhy_color", true )
-
-// : $content['value']['color'];
-
-// $typo_graphy['color'] == '' ? $typo_graphy['color'] = $content['value']['color'] : $typo_graphy['color'];
-
-// // font family
-
-// $typo_graphy['family'] = ( $this->current_screen == "post" ) && !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_family", true ) ) ?
-
-// get_post_meta( $post->ID, $this->prefix . "typograhy_family", true )
-
-// : $content['value']['family'];
-
-// $typo_graphy['family'] == '' ? $typo_graphy['family'] = $content['value']['family'] : $typo_graphy['family'];
-
-// // font style
-
-// $typo_graphy['style'] = ( $this->current_screen == "post" ) && !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_style", true ) ) ?
-
-// get_post_meta( $post->ID, $this->prefix . "typograhy_style", true )
-
-// : $content['value']['style'];
-
-// $typo_graphy['style'] == '' ? $typo_graphy['style'] = $content['value']['style'] : $typo_graphy['style'];
-
-// // font weight
-
-// $typo_graphy['weight'] = ( $this->current_screen == "post" ) && !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_weight", true ) ) ?
-
-// get_post_meta( $post->ID, $this->prefix . "typograhy_weight", true )
-
-// : $content['value']['weight'];
-
-// $typo_graphy['weight'] == '' ? $typo_graphy['weight'] = $content['value']['weight'] : $typo_graphy['weight'];
-
-// // font size
-
-// $typo_graphy['size'] = ( $this->current_screen == "post" ) && !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_size", true ) ) ?
-
-// get_post_meta( $post->ID, $this->prefix . "typograhy_size", true )
-
-// : $content['value']['size'];
-
-// $typo_graphy['size'] == '' ? $typo_graphy['size'] = $content['value']['size'] : $typo_graphy['size'];
-
-// // line-height
-
-// $typo_graphy['line-height'] = ( $this->current_screen == "post" ) && !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_line_height", true ) ) ?
-
-// get_post_meta( $post->ID, $this->prefix . "typograhy_line_height", true )
-
-// : $content['value']['line-height'];
-
-// $typo_graphy['line-height'] == '' ? $typo_graphy['line-height'] = $content['value']['line-height'] : $typo_graphy['line-height'];
-
-// // letter-spacing
-
-// $typo_graphy['letter-spacing'] = ( $this->current_screen == "post" ) && !is_null( get_post_meta( $post->ID, $this->prefix . "typograhy_letter_spacing", true ) ) ?
-
-// get_post_meta( $post->ID, $this->prefix . "typograhy_letter_spacing", true )
-
-// : $content['value']['letter-spacing'];
-
-// $typo_graphy['letter-spacing'] == '' ? $typo_graphy['letter-spacing'] = $content['value']['letter-spacing'] : $typo_graphy['letter-spacing'];
-
-        // $this->value = $typo_graphy;
 
         $this->value = (  ( $this->current_screen == "post" )
             && !is_null( get_post_meta( $post->ID, $this->prefix . $content['name'], true ) )
@@ -160,7 +86,6 @@ class Typography extends Structure {
 
         echo "<div>" . esc_html( $label ) . "</div>";
         echo "<div><small>" . esc_html( $desc ) . "</small></div>";
-        $value = $this->value;
 
         foreach ( $components as $key => $item ) {
 
@@ -276,7 +201,144 @@ break;
     }
 
     public function edit_fields( $term, $taxonomy ) {
-    }
+
+        $name  = isset( $this->content['name'] ) ? $this->prefix . $this->content['name'] : "";
+        $value = maybe_unserialize( get_term_meta( $term->term_id, $name, true ) );
+
+        $this->load_scripts();
+
+        $font_list             = $this->dm_getGoogleFonts();
+        $data['font_list']     = $font_list;
+        $data['selected_data'] = $value;
+        wp_localize_script( 'dm-typo-script-handle', 'typo_config', $data );
+
+        $label = isset( $this->content['label'] ) ? $this->content['label'] : '';
+        $desc  = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
+        $attrs = isset( $this->content['attr'] ) ? $this->content['attr'] : '';
+        $components = isset( $this->content['components'] ) ? $this->content['components'] : [];
+
+        $default_attributes = "";
+        $dynamic_classes    = "";
+
+        if ( is_array( $attrs ) && !empty( $attrs ) ) {
+
+            foreach ( $attrs as $key => $val ) {
+
+                if ( $key == "class" ) {
+                    $dynamic_classes .= $val . " ";
+                } else {
+                    $default_attributes .= $key . "='" . $val . "' ";
+                }
+
+            }
+
+        }
+
+        $class_attributes = "class='dm-option term-group-wrap $dynamic_classes'";
+        $default_attributes .= $class_attributes;
+
+        ?>
+
+<tr <?php echo dm_render_markup( $default_attributes ); ?> >
+    <th scope="row">
+        <label class="dm-option-label"><?php echo esc_html( $label ); ?></label>
+    </th>
+    <td>
+
+        <!-- typography markup starts -->
+        <?php
+
+        foreach ( $components as $key => $item ) {
+
+            if ( $key ) {
+
+                switch ( $key ) {
+                case 'family':
+                    echo "<div>" . esc_html( 'Family:' ) . "</div>";
+
+                    if ( count( $font_list ) > 0 ) {
+                        ?>
+                <div class="google_fonts_select_control">
+                    <div class="google-fonts">
+                        <select class="google-fonts-list" name="<?php echo esc_attr( $name ) ?>[family]">
+                            <?php
+
+                        foreach ( $font_list as $key => $item ) {
+                            $selected = $item->family == esc_html( $value["family"] ) ? 'selected' : '';
+                            echo '<option value="' . $item->family . '" ' . $selected . '>' . $item->family . '</option>';
+                        }
+
+                        ?>
+                        </select>
+                    </div>
+                    <div class="weight">
+                        <label>Weight</label>
+                        <select name="<?php echo esc_attr( $name ) ?>[weight]" class="google-weight-list">
+                        </select>
+                    </div>
+                    <div class="style">
+                        <label>Style</label>
+                        <select name="<?php echo esc_attr( $name ) ?>[style]" class="google-style-list">
+                        </select>
+                    </div>
+                </div>
+                <?php
+}
+
+                    break;
+                case 'size':
+                    echo "<div>" . esc_html( 'Size :' ) . "</div>";
+                    ?>
+            <input class="range-slider-font-size dm_group_typhography_range_font_size" type="range" min="0" max="100"   value="<?php echo isset( $value["size"] ) ? esc_html( floatval( $value["size"] ) ) : 0.00; ?>">
+            <input type="text" name="<?php echo esc_attr( $name ) ?>[size]"
+            value="<?php echo isset( $value["size"] ) ? esc_html( trim( $value["size"] ) ) : 0.00; ?>"  id="size_value" />
+            <?php
+break;
+                case 'line-height':
+                    echo "<div>" . esc_html( 'Line height:' ) . "</div>";
+                    ?>
+            <input class="range-slider-line-height dm_group_typhography_line_height" type="range" min="0" max="100"  value="<?php echo isset( $value["line_height"] ) ? esc_html( floatval( $value["line_height"] ) ) : 0.00; ?>">
+            <input type="text"name="<?php echo esc_attr( $name ) ?>[line_height]"
+            value="<?php echo isset( $value["line_height"] ) ? esc_html( trim( $value["line_height"] ) ) : 0.00; ?>"   id="line_height_value" />
+            <?php
+break;
+                case 'letter-spacing':
+                    echo "<div>" . esc_html( 'Later space:' ) . "</div>";
+                    ?>
+               <input class="range-slide-letter-space dm_group_typhography_letterspace" type="range" min="-10" max="10"   value="<?php echo isset( $value["letter_spacing"] ) ? esc_html( floatval( $value["letter_spacing"] ) ) : 0.00; ?>">
+               <input type="text" name="<?php echo esc_attr( $name ) ?>[letter_spacing]"
+            value="<?php echo isset( $value["letter_spacing"] ) ? esc_html( trim( $value["letter_spacing"] ) ) : 0.00; ?>" id="latter_spacing_value" />
+            <?php
+break;
+                case 'color':
+                    echo "<div>" . esc_html( 'Color:' ) . "</div>";
+                    ?>
+            <input  type="text"
+            name="<?php echo esc_attr( $name ) ?>[color]"
+            value="<?php echo isset( $value["color"] ) ? esc_html( $value["color"] ) : ""; ?>"
+            class="dm-typography-color-field"
+            data-default-color="<?php echo esc_attr( $value["color"] ); ?>" />
+            <?php
+break;
+
+                default:
+                    # code...
+                    break;
+                }
+
+            }
+
+        }
+
+        ?>
+        <!-- typography markup ends -->
+
+
+        <br><small class="dm-option-desc">(<?php echo esc_html( $desc ); ?> )</small>
+    </td>
+</tr>
+<?php
+}
 
     /**
      * Return the list of Google Fonts from our json file. Unless otherwise specfied, list will be limited to 30 fonts.
