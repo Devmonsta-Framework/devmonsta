8cb682c010078567b6ee6075f981cc53bad4d974
easin-xs <easin@xpeedstudio.com>
2020-05-12 18:21:13 +0600

    working on icon picker

--- core/options/posts/controls/icon/assets/js/script.js
+++ core/options/posts/controls/icon/assets/js/script.js
@@ -1,6 +1,69 @@
+Vue.component('dm-icon-picker',{
+    props: ["icon_list", "name"],
+    template: `
+        <div class="dm-icon-control">
+            <div class="dm-select-icon">
+                <div class="dm-icon-box">
+                    <i class="current-icon fas fa-eye"></i>
+                </div>
+                <button class="dm-add-icon-btn button">Add Icon</button>
+                <input type="hidden" :name="name">
+            </div>
+            <dm-icon-modal :iconList="iconList"></dm-icon-modal>
+        </div>
+    `,
+    data: function(){
+        return {
+            iconList: []
+        }
+    },
+    created: function(){
+        this.iconList = JSON.parse(this.icon_list)
+    }
+});
+
+Vue.component('dm-icon-modal', {
+    props: ["iconList"],
+    template: `
+        <div class="dm-icon-modal-container">
+            <div class="dm-icon-modal-data">
+                <div class="dm-icon-modal-header">
+                    <ul>
+                        <li>Icon Fonts</li>
+                    </ul>
+                </div>
+                <div class="dm-icon-modal-selection">
+                    <select class="dm-icon-type" v-if="iconList.length">
+                        <option :value="icon.name.toLowerCase().replace(/\s/g, '-')" v-for="icon in iconList">{{ icon.name }}</option>
+                    </select>
+                    <input type="text" placeholder="serach..." class="dm-icon-search">
+                </div>
+                <dm-icon-list v-if="iconList.length" v-for="icon in iconList" :icons="icon.icons"></dm-icon-list>
+                <div class="dm-icon-modal-footer">
+                    <button class="button media-button button-primary button-large media-button-0">Save</button>
+                </div>
+            </div>
+        </div>
+    `,
+});
+
+Vue.component('dm-icon-list', {
+    props: ["icons"],
+    template: `
+        <div class="dm-list-icon">
+            <ul>
+                <li :data-icon="icon" v-for="icon in icons"><span :class="icon"></span></li>
+            </ul>
+        </div>
+    `
+});
+
 var app = new Vue({
     el: '.dm-vue-app',
     data: {
-        message: "hello world"
+        icons: []
+    },
+    created: function(){
+        this.icons = dmIcons;
     }
 })
--- core/options/posts/controls/icon/icon.php
+++ core/options/posts/controls/icon/icon.php
@@ -21,9 +21,13 @@ class Icon extends Structure {
     }
 
     public function enqueue_icon_scripts() {
+        include 'icon-data.php';
+
         wp_enqueue_style( 'dm-fontawesome-css', DM_CORE . 'options/posts/controls/icon/assets/css/font-awesome.min.css' );
-        wp_enqueue_script( 'vue-js', 'https://cdn.jsdelivr.net/npm/vue' );
-        wp_enqueue_script( 'dm-asicon', DM_CORE . 'options/posts/controls/icon/assets/js/script.js', ['jquery'], time(), true );
+        wp_enqueue_style( 'dm-main-css', DM_CORE . 'options/posts/controls/icon/assets/css/main.css' );
+        wp_enqueue_script( 'vue-js', 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js' );
+        wp_enqueue_script( 'dm-asicon', DM_CORE . 'options/posts/controls/icon/assets/js/script.js', ['jquery', 'vue-js'], time(), true );
+        wp_localize_script( 'dm-asicon', 'dmIcons', $iconList );
     }
 
     /**
@@ -43,6 +47,8 @@ class Icon extends Structure {
      * @internal
      */
     public function output() {
+        include 'icon-data.php';
+
         $label              = isset( $this->content['label'] ) ? $this->content['label'] : '';
         $name               = isset( $this->content['name'] ) ? $this->content['name'] : '';
         $desc               = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
@@ -50,27 +56,7 @@ class Icon extends Structure {
         $default_attributes = "";
         $dynamic_classes    = "";
 
-        $iconList = [
-                [
-                    "name" => "Font Awesome v5.0.1",
-                    "icons" => [
-                        "fab fa-500px",
-                        "fab fa-accessible-icon",
-                        "fab fa-accusoft",
-                        "fas fa-address-book", "far fa-address-book",
-                        "fas fa-address-card", "far fa-address-card",
-                        "fas fa-adjust",
-                        "fab fa-adn",
-                        "fab fa-adversal",
-                        "fab fa-affiliatetheme",
-                        "fab fa-algolia",
-                        "fas fa-align-center",
-                        "fas fa-align-justify",
-                        "fas fa-align-left",
-                        "fas fa-align-right",
-                    ]
-                ]
-        ];
+        
 
         if ( is_array( $attrs ) && !empty( $attrs ) ) {
 
@@ -86,36 +72,23 @@ class Icon extends Structure {
 
         }
 
-        $class_attributes = "class='dm-vue-app dm-option $dynamic_classes'";
+        $class_attributes = "class='dm-vue-app dm-option form-field $dynamic_classes'";
         $default_attributes .= $class_attributes;
-
+        $iconEncoded = json_encode($iconList);
         ?>
         
         <div <?php echo dm_render_markup( $default_attributes ); ?> >
-            <label class="dm-option-label"><?php echo esc_html( $label ); ?> {{ message }}</label>
-            <div><small class="dm-option-desc"><?php echo esc_html( $desc ); ?> </small></div>
-            <select id="default" name="<?php echo esc_attr( $this->prefix . $name ); ?>" class="dm-icon-picker">
-                            <option value="fa-search">Search</option>
-                            <option value="fa-star">Star</option>
-                            <option value="fa-times">Times</option>
-                            <option value="fa-refresh">Refresh</option>
-                            <option value="fa-rocket">Rocket</option>
-                            <option value="fa-bookmark">Bookmark</option>
-                            <option value="fa-heart">Heart</option>
-                            <option value="fa-adn">Adn</option>
-                            <option value="fa-cloud-upload">Cloud-upload</option>
-                            <option value="fa-phone-square">Phone-square</option>
-                            <option value="fa-caret-right">Caret-right</option>
-                            <option value="fa-caret-down">Caret-down</option>
-                            <option value="fa-caret-up">Caret-up</option>
-                            <option value="fa-caret-left">Caret-left</option>
-                            <option value="fa-eye">Eye</option>
-                            <option value="fa-tag">Tag</option>
-                            <option value="fa-cog">Cog</option>
-                            <option value="fa-wrench">Wrench</option>
-                            <option value="fa-volume-down">Volume-down</option>
-                            <option value="fa-thumbs-up">Thumbs-up</option>
-            </select>
+            <div class="dm-option-column left">
+                <label class="dm-option-label"><?php echo esc_html( $label ); ?></label>
+            </div>
+            <div class="dm-option-column right">
+                <dm-icon-picker
+                    name='<?php echo esc_attr( $this->prefix . $name ); ?>'
+                    title="hello world"
+                    icon_list='<?php echo $iconEncoded; ?>'
+                ></dm-icon-picker>
+                <p class="dm-option-desc"><?php echo esc_html( $desc ); ?> </p>
+            </div>
         </div>
     <?php
 }
