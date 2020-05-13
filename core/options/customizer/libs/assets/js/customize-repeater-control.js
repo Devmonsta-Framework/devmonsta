f8f8da9ebc9fc0f836864a0a1d051ccbc6d3ceac
Prappo <prappo@xpeedstudio.com>
2020-05-13 15:05:56 +0600

    Custoizer update

--- core/options/customizer/libs/assets/js/customize-repeater-control.js
+++ core/options/customizer/libs/assets/js/customize-repeater-control.js
@@ -1,243 +1,223 @@
-(function(api, wp, $) {
-    'use strict';
+(function (api, wp, $) {
+	'use strict';
 
-    wp.customize.control.add(new wp.customize.DateTimeControl('birthdate', {
-        label: 'Birthdate',
-        description: "Someone was born on this day.",
-        section: 'devmonsta_text_settings_section',
-        includeTime: false,
-        setting: new wp.customize.Value('2000-01-02'),
-        transport: 'postMessage',
-    }));
 
 
+	api.RepeaterControl = api.RepeaterControl || {};
 
+	api.RepeaterControl = api.Control.extend({
+		ready: function ready() {
 
-    api.RepeaterControl = api.RepeaterControl || {};
+			wp.customize.control.each(function (control) {
+				// console.log(control);
+			});
 
-    api.RepeaterControl = api.Control.extend({
-        ready: function ready() {
 
-            wp.customize.control.each(function(control) {
-                // console.log(control);
-            });
 
+			var control = this;
 
+			_.bindAll(this, 'addRepeaterRow', 'deleteRepeaterRow', 'createRowSetting', 'normalizeSaveRequestQuery');
 
-            var control = this;
+			this.STATE = [],
+				this.fieldQueryPattern = new RegExp('^' + control.id + '_(\\d+)_(\\S+)$');
+			this.rowPrototype = this.container.find('.customize-control-repeater-field.prototype').remove().clone().removeClass('prototype')
+			this.btnNew = this.container.find('.customize-add-repeater-field');
 
-            _.bindAll(this, 'addRepeaterRow', 'deleteRepeaterRow', 'createRowSetting', 'normalizeSaveRequestQuery');
+			this.setupRepeaterRows();
 
-            this.STATE = [],
-                this.fieldQueryPattern = new RegExp('^' + control.id + '_(\\d+)_(\\S+)$');
-            this.rowPrototype = this.container.find('.customize-control-repeater-field.prototype').remove().clone().removeClass('prototype')
-            this.btnNew = this.container.find('.customize-add-repeater-field');
+			// Remove repeater fields from save request (@todo: Is there a better way?)
+			api.bind('save-request-params', this.normalizeSaveRequestQuery);
 
-            this.setupRepeaterRows();
+			this.btnNew.on('click', this.addRepeaterRow);
+			this.container.on('click', '.customize-control-repeater-field .menu-item-handle', this.toggleFieldSettings);
+			this.container.on('click', '.customize-control-repeater-field .item-delete', this.deleteRepeaterRow);
 
-            // Remove repeater fields from save request (@todo: Is there a better way?)
-            api.bind('save-request-params', this.normalizeSaveRequestQuery);
+			// on sortable change
 
-            this.btnNew.on('click', this.addRepeaterRow);
-            this.container.on('click', '.customize-control-repeater-field .menu-item-handle', this.toggleFieldSettings);
-            this.container.on('click', '.customize-control-repeater-field .item-delete', this.deleteRepeaterRow);
+			$('.customize-control-content').sortable({
+				axis: 'y',
+				update: function () {
 
-            // on sortable change
+					control.saveData();
 
-            $('.customize-control-content').sortable({
-                axis: 'y',
-                update: function() {
+				}
+			});
 
-                    control.saveData();
 
-                }
-            });
+		},
 
+		saveData: function () {
 
-        },
+			// wp.customize.previewer.refresh();
+			// person_name
+			var control = this, rows;
 
-        saveData: function() {
+			try {
+				rows = JSON.parse(control.setting.get());
+				console.log(rows);
+			} catch (e) {
+				return false;
+			}
 
-            // wp.customize.previewer.refresh();
-            // person_name
-            var control = this,
-                rows;
+			$.each(rows, function (index, value) {
+				control.addRepeaterRow(rows);
 
-            try {
-                rows = JSON.parse(control.setting.get());
-                console.log(rows);
-            } catch (e) {
-                return false;
-            }
+			});
+			console.log('data saved');
+	
+		}
+		,
+		setupRepeaterRows: function () {
+			var control = this, rows;
 
-            $.each(rows, function(index, value) {
-                control.addRepeaterRow(rows);
+			try {
+				rows = JSON.parse(control.setting.get());
+				console.log(rows);
+			} catch (e) {
+				return false;
+			}
 
-            });
-            console.log('data saved');
+			$.each(rows, function (index, value) {
+				control.addRepeaterRow(rows);
 
-        },
-        setupRepeaterRows: function() {
-            var control = this,
-                rows;
+			});
+		},
 
-            try {
-                rows = JSON.parse(control.setting.get());
-                console.log(rows);
-            } catch (e) {
-                return false;
-            }
+		addRepeaterRow: function (event) {
+			var control = this, Constructor, Control, options, size, index, row, setting, stateField, initialState, fieldLabel;
 
-            $.each(rows, function(index, value) {
-                control.addRepeaterRow(rows);
+			if (!(event instanceof Event)) {
+				initialState = event;
+			}
 
-            });
-        },
+			size = control.STATE.push({});
+			index = size - 1;
+			stateField = control.STATE[index];
 
-        addRepeaterRow: function(event) {
-            var control = this,
-                Constructor, Control, options, size, index, row, setting, stateField, initialState, fieldLabel;
+			row = control.rowPrototype.clone().insertBefore(control.btnNew);
+			row.data('index', index);
 
-            if (!(event instanceof Event)) {
-                initialState = event;
-            }
+			fieldLabel = row.find('.menu-item-title');
 
-            size = control.STATE.push({});
-            index = size - 1;
-            stateField = control.STATE[index];
+			// console.log('Fields');
+			// console.log(control.params.fields);
 
-            row = control.rowPrototype.clone().insertBefore(control.btnNew);
-            row.data('index', index);
+			$.each(control.params.fields, function (key, field) {
+				var id, defaultValue = '', label;
 
-            fieldLabel = row.find('.menu-item-title');
+				if (initialState && initialState[index] && initialState[index].hasOwnProperty(key)) {
+					defaultValue = initialState[index][key];
+				}
 
-            // console.log('Fields');
-            // console.log(control.params.fields);
+				// Set id
+				id = control.id + '_' + index + '_' + key;
 
+				// Create new setting
+				setting = control.createRowSetting(id, defaultValue);
 
+				stateField[key] = setting.get();
 
-            $.each(control.params.fields, function(key, field) {
+				// Watch setting
+				setting.bind(_.bind(control.watchFieldValue, { control: control, index: index, key: key }));
 
-                console.log(field);
+				// Reset field arguments (@todo: media controls have args['attachment'] that gets filled by its value and stored in _wpCustomizeSettings[Controls], but we can't pass them from server to client without loading every control individual)
+				field.args = _.extend(field.args, {
+					content: null,
+					priority: 10 + index,
+					settings: {
+						'default': id
+					}
+				});
 
-                var id, defaultValue = '',
-                    label;
+				// Set attachment for media controls
+				if (field.attachments && 'image' === field.args.type) {
+					field.args.attachment = field.attachments[index] || null
+				}
 
-                if (initialState && initialState[index] && initialState[index].hasOwnProperty(key)) {
-                    defaultValue = initialState[index][key];
-                }
+				Constructor = api[field.control] || api.Control;
+				options = _.extend({ params: field.args }, field.args);
+				Control = new Constructor(id, options);
 
-                // Set id
-                id = control.id + '_' + index + '_' + key;
+				//api.control.add( Control ); //@todo: disabled so far, because it manipulates our previous dom settings
+				setting.preview();
 
-                // Create new setting
-                setting = control.createRowSetting(id, defaultValue);
+				// Add template data
+				label = stateField[key] || field.args.label
+				if (control.params.labelField && key === control.params.labelField && label) {
+					fieldLabel.html(label)
+				}
 
-                stateField[key] = setting.get();
 
-                // Watch setting
-                setting.bind(_.bind(control.watchFieldValue, { control: control, index: index, key: key }));
+				// console.log(Control.container);
 
-                // Reset field arguments (@todo: media controls have args['attachment'] that gets filled by its value and stored in _wpCustomizeSettings[Controls], but we can't pass them from server to client without loading every control individual)
-                field.args = _.extend(field.args, {
-                    content: null,
-                    priority: 10 + index,
-                    settings: {
-                        'default': id
-                    }
-                });
+				// Add field to row
+				row.find('.customize-control-repeater-field-settings').append(Control.container);
 
-                // Set attachment for media controls
-                if (field.attachments && 'image' === field.args.type) {
-                    field.args.attachment = field.attachments[index] || null
-                }
 
-                Constructor = api[field.control] || api.Control;
-                options = _.extend({ params: field.args }, field.args);
-                Control = new Constructor(id, options);
 
-                //api.control.add( Control ); //@todo: disabled so far, because it manipulates our previous dom settings
-                setting.preview();
+			});
+		},
 
-                // Add template data
-                label = stateField[key] || field.args.label
-                if (control.params.labelField && key === control.params.labelField && label) {
-                    fieldLabel.html(label)
-                }
+		createRowSetting: function (id, defaultValue) {
+			var control = this, setting;
 
+			setting = api.create(id, id, {}, {
+				type: 'repeater',
+				transport: control.settings.default.transport,
+				previewer: api.previewer
+			});
+			setting.set(defaultValue);
 
-                // console.log(Control.container);
+			return setting;
+		},
 
-                // Add field to row
-                row.find('.customize-control-repeater-field-settings').append(Control.container);
+		deleteRepeaterRow: function (event) {
+			var row = $(event.target).closest('.customize-control-repeater-field');
+			var index = row.data('index');
 
+			this.STATE.splice(index, 1);
+			this.setting.set(JSON.stringify(this.STATE));
 
+			row.remove();
+		},
 
-            });
-        },
+		watchFieldValue: function (value) {
+			this.control.STATE[this.index][this.key] = value;
+			this.control.setting.set(JSON.stringify(this.control.STATE));
+		},
 
-        createRowSetting: function(id, defaultValue) {
-            var control = this,
-                setting;
+		normalizeSaveRequestQuery: function (query) {
+			var control = this, changes = query.customized || query.customize_changeset_data;
 
-            setting = api.create(id, id, {}, {
-                type: 'repeater',
-                transport: control.settings.default.transport,
-                previewer: api.previewer
-            });
-            setting.set(defaultValue);
+			try {
+				changes = JSON.parse(changes);
+				// console.log(changes);
+			} catch (e) {
+				return;
+			}
 
-            return setting;
-        },
+			$.each(changes, function (key, value) {
+				if (control.fieldQueryPattern.exec(key)) {
+					delete changes[key];
+				}
+			});
 
-        deleteRepeaterRow: function(event) {
-            var row = $(event.target).closest('.customize-control-repeater-field');
-            var index = row.data('index');
+			if (query.hasOwnProperty('customized')) {
+				query.customized = JSON.stringify(changes);
+			} else if (query.hasOwnProperty('customize_changeset_data')) {
+				query.customize_changeset_data = JSON.stringify(changes);
+			}
 
-            this.STATE.splice(index, 1);
-            this.setting.set(JSON.stringify(this.STATE));
+		},
 
-            row.remove();
-        },
+		toggleFieldSettings: function () {
+			$(this).closest('.menu-item').toggleClass('menu-item-edit-inactive').toggleClass('menu-item-edit-active')
+		}
+	});
 
-        watchFieldValue: function(value) {
-            this.control.STATE[this.index][this.key] = value;
-            this.control.setting.set(JSON.stringify(this.control.STATE));
-        },
-
-        normalizeSaveRequestQuery: function(query) {
-            var control = this,
-                changes = query.customized || query.customize_changeset_data;
-
-            try {
-                changes = JSON.parse(changes);
-                // console.log(changes);
-            } catch (e) {
-                return;
-            }
-
-            $.each(changes, function(key, value) {
-                if (control.fieldQueryPattern.exec(key)) {
-                    delete changes[key];
-                }
-            });
-
-            if (query.hasOwnProperty('customized')) {
-                query.customized = JSON.stringify(changes);
-            } else if (query.hasOwnProperty('customize_changeset_data')) {
-                query.customize_changeset_data = JSON.stringify(changes);
-            }
-
-        },
-
-        toggleFieldSettings: function() {
-            $(this).closest('.menu-item').toggleClass('menu-item-edit-inactive').toggleClass('menu-item-edit-active')
-        }
-    });
-
-    $.extend(api.controlConstructor, {
-        repeater: api.RepeaterControl
-    });
+	$.extend(api.controlConstructor, {
+		repeater: api.RepeaterControl
+	});
 
 
 
@@ -247,20 +227,20 @@
 })(wp.customize, wp, jQuery);
 
 
-(function($) {
-    var api = wp.customize;
+(function ($) {
+	var api = wp.customize;
 
-    api.SliderControl = api.Control.extend({
-        ready: function() {
+	api.SliderControl = api.Control.extend({
+		ready: function () {
+			
+			console.log('slider');
+		}
+	});
 
-            console.log('slider');
-        }
-    });
+	jQuery.extend(api.controlConstructor, {
+		'test-control': api.SliderControl
+	});
 
-    jQuery.extend(api.controlConstructor, {
-        'test-control': api.SliderControl
-    });
-
-    // api.controlConstructor['test-control'] = api.SliderControl;
+	// api.controlConstructor['test-control'] = api.SliderControl;
 
 })(jQuery);
