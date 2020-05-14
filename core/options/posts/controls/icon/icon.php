5e69b4ee6300f764dec548bcc0378518a04d2f51
Reza Khan <rezakhan995@gmail.com>
2020-05-14 16:38:36 +0600

    icons updated

--- core/options/posts/controls/icon/icon.php
+++ core/options/posts/controls/icon/icon.php
@@ -39,7 +39,8 @@ class Icon extends Structure {
         global $post;
 
         $icon_data              = [];
-        $icon_data['icon_name'] = (  ( $this->current_screen == "post" ) && ( "" != get_post_meta( $post->ID, $this->prefix . $this->content['name'], true ) )
+        $icon_data['icon_name'] = (  ( $this->current_screen == "post" )
+            && ( "" != get_post_meta( $post->ID, $this->prefix . $this->content['name'], true ) )
             && !is_null( get_post_meta( $post->ID, $this->prefix . $this->content['name'], true ) ) )
         ? get_post_meta( $post->ID, $this->prefix . $this->content['name'], true )
         : "";
@@ -48,7 +49,6 @@ class Icon extends Structure {
             && !is_null( get_post_meta( $post->ID, $this->prefix . $this->content['name'] . "_type", true ) ) )
         ? get_post_meta( $post->ID, $this->prefix . $this->content['name'] . "_type", true )
         : "dm-font-awesome";
-        // wp_localize_script( 'dm-asicon', 'iconValues', $data );
 
         $this->value = $icon_data;
         $this->output();
@@ -103,10 +103,86 @@ class Icon extends Structure {
 }
 
     public function columns() {
+        $visible = false;
+        $content = $this->content;
+        add_filter( 'manage_edit-' . $this->taxonomy . '_columns',
+            function ( $columns ) use ( $content, $visible ) {
 
+                $visible = ( isset( $content['show_in_table'] ) && $content['show_in_table'] === true ) ? true : false;
+
+                if ( $visible ) {
+                    $columns[$content['name']] = __( $content['label'], 'devmonsta' );
+                }
+
+                return $columns;
+            } );
+
+        $cc = $content;
+        add_filter( 'manage_' . $this->taxonomy . '_custom_column',
+            function ( $content, $column_name, $term_id ) use ( $cc ) {
+
+                if ( $column_name == $cc['name'] ) {
+                    echo get_term_meta( $term_id, 'devmonsta_' . $column_name, true );
+
+                }
+
+                return $content;
+
+            }, 10, 3 );
     }
 
     public function edit_fields( $term, $taxonomy ) {
-    }
+        $this->enqueue_icon_scripts();
+
+        include 'icon-data.php';
+        $label              = isset( $this->content['label'] ) ? $this->content['label'] : '';
+        $prefix             = 'devmonsta_';
+        $name               = isset( $this->content['name'] ) ? $prefix . $this->content['name'] : '';
+        $desc               = isset( $this->content['desc'] ) ? $this->content['desc'] : '';
+        $attrs              = isset( $this->content['attr'] ) ? $this->content['attr'] : '';
+        $icon               = get_term_meta( $term->term_id, $name, true );
+        $icon_type          = get_term_meta( $term->term_id, $name . "_type", true );
+        $default_attributes = "";
+        $dynamic_classes    = "";
+
+        $default_attributes = "";
+        $dynamic_classes    = "";
+
+        if ( is_array( $attrs ) && !empty( $attrs ) ) {
+
+            foreach ( $attrs as $key => $val ) {
+
+                if ( $key == "class" ) {
+                    $dynamic_classes .= $val . " ";
+                } else {
+                    $default_attributes .= $key . "='" . $val . "' ";
+                }
+
+            }
+
+        }
+
+        $class_attributes = "class='dm-vue-app dm-option form-field $dynamic_classes'";
+        $default_attributes .= $class_attributes;
+        $iconEncoded = json_encode( $iconList );
+
+        ?>
+
+            <tr <?php echo dm_render_markup( $default_attributes ); ?> >
+                <th scope="row">
+                    <label class="dm-option-label"><?php echo esc_html( $label ); ?></label>
+                </th>
+                <td>
+                    <dm-icon-picker
+                        name='<?php echo esc_attr( $name ); ?>'
+                        icon_list='<?php echo $iconEncoded; ?>'
+                        default_icon_type='<?php echo isset( $icon_type ) ? $icon_type : "dm-font-awesome"; ?>'
+                        default_icon='<?php echo isset( $icon ) ? $icon : ""; ?>'
+                    ></dm-icon-picker>
+                    <br><small class="dm-option-desc">(<?php echo esc_html( $desc ); ?> )</small>
+                </td>
+            </tr>
+<?php
+}
 
 }
