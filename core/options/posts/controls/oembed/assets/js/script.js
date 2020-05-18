24bd9398de62cb4d3167d4262b241f1b9dde2178
Reza Khan <rezakhan995@gmail.com>
2020-05-18 14:41:02 +0600

    oembed js update

--- core/options/posts/controls/oembed/assets/js/script.js
+++ core/options/posts/controls/oembed/assets/js/script.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function($){
 
-    console.log("oembed");
     var ajaxurl = object.ajaxurl;
-    console.log(ajaxurl);
     var is_url = function(str) {
 	    var pattern = new RegExp(/^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/, 'i');
 	    return pattern.test(str);
@@ -11,16 +9,12 @@ jQuery(document).ready(function($){
    
     $(".dm-oembed-url-input").on("keyup", 
         _.debounce(function(){
-            console.log("key pressed");
             var wrapper = $(this);
             
             var url_input = $(this).val();
-            console.log(url_input);
 
             var iframeWrapper = wrapper.siblings(".dm-oembed-preview");
             if( url_input && is_url( url_input ) ) {
-                console.log(wrapper.data('nonce'));
-                console.log(wrapper.data('preview'));
                 var data = {
                     action : 'get_oembed_response', 
                     _nonce : wrapper.data('nonce'),
--- core/options/posts/controls/oembed/oembed.php
+++ core/options/posts/controls/oembed/oembed.php
@@ -79,7 +79,6 @@ class Oembed extends Structure {
 
         $class_attributes = "class='dm-option $dynamic_classes'";
         $default_attributes .= $class_attributes;
-        return false;
         ?>
 
 
