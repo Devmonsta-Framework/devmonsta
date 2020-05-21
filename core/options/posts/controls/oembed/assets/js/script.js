59f944c8dcad73a8bb399b897e5dbd2250c0342a
Prappo Prince <prappo@xpeedstudio.com>
2020-05-21 11:16:04 +0600

    Update script.js

--- core/options/posts/controls/oembed/assets/js/script.js
+++ core/options/posts/controls/oembed/assets/js/script.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function($) {
-
     var ajaxurl = object.ajaxurl;
     var is_url = function(str) {
         var pattern = new RegExp(/^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/, 'i');
@@ -15,14 +14,21 @@ jQuery(document).ready(function($) {
 
             var iframeWrapper = wrapper.siblings(".dm-oembed-preview");
             if (url_input && is_url(url_input)) {
+
                 var data = {
-                    action: 'get_oembed_response',
+                    action: "get_oembed_response",
                     _nonce: wrapper.data('nonce'),
                     preview: wrapper.data('preview'),
                     url: url_input
                 };
-                $.post(ajaxurl, data, function(response) {
-                    iframeWrapper.html(response);
+
+                $.ajax({
+                    type: "POST",
+                    url: ajaxurl,
+                    data: data,
+                    success: function(response) {
+                        iframeWrapper.html(response);
+                    },
                 });
             } else {
                 iframeWrapper.html('');
