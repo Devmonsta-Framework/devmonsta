fb288e5fa689722a7e05b6c7ba7380cfe11f62fe
easin-xs <easin@xpeedstudio.com>
2020-06-02 12:10:35 +0600

    working on repeater

--- core/options/posts/assets/js/repeater.js
+++ core/options/posts/assets/js/repeater.js
@@ -1,26 +1,56 @@
 jQuery(document).ready(function ($) {
-    jQuery('.dm-repeater-add-new').on('click', function(e){
+    function controlNameChanging(param){
+        $(param.repeaterControl).each(function(){
+            $(this).find('.dm-ctrl').each(function(index){
+                var clonedInputs = !param.isRemoved ? param.clonedElement.find('.dm-ctrl')[index] : this;
+                if(!param.isRemoved){
+                    repeatCount = index;
+                }
+    
+                var name = $(clonedInputs).attr('name') ? $(clonedInputs).attr('name') : '';
+                if(name){
+                    $(clonedInputs).attr('name', 'dm_options['+ param.id +']['+ param.repeatCount +']['+ name +']')
+                }
+            });
+        })
+        
+    }
+    
+    jQuery('.dm-repeater-add-new').on('click', function(e, isRemoved){
         e.preventDefault();
         var id = $(this).data('id');
 
         var repeaterControl = $('.dm-repeater-sample'),
             clonedElement = repeaterControl.clone().removeClass('dm-repeater-sample'),
-            repeatCount = $(this).parents('.dm-repeater-column').find('.dm-repeater-control-list').children.length + 1;
-            console.log($(this).parents('.dm-repeater-column').find('.dm-repeater-control-list'));
+            repeatCount = $(this).parents('.dm-repeater-column').find('.dm-repeater-control-list').children().length + 1;
 
             clonedElement.find('.dm-repeater-control-action, .dm-repeater-popup-close, .dm-editor-post-trash').attr('data-id', id+'_'+(repeatCount)).end();
             clonedElement.find('.dm-repeater-inner-controls').attr('id', id+'_'+(repeatCount)).end();
 
-        $(this).parents('.dm-repeater-column').find('.dm-repeater-control-list').append(clonedElement);
+        var controlConfig = {
+            'repeaterControl': [repeaterControl],
+            'isRemoved': isRemoved,
+            'clonedElement': clonedElement,
+            'id': id,
+            'repeatCount': repeatCount,
+        };
+        if(!isRemoved){
+            $(this).parents('.dm-repeater-column').find('.dm-repeater-control-list').append(clonedElement);
+        } else {
+            repeatCount = repeatCount - 1;
+        }
+
 
         // resetting repeater name
-        repeaterControl.find('.dm-ctrl').each(function(index){
-            console.log(index);
-            var name = $(this).attr('name') ? $(this).attr('name') : '';
-            if(name){
-                $(this).attr('name', 'dm_options['+ id +']['+ repeatCount +']['+ name +']')
-            }
-        });
+        controlNameChanging(controlConfig)
+        // repeaterControl.find('.dm-ctrl').each(function(index){
+        //     var clonedInputs = !isRemoved ? clonedElement.find('.dm-ctrl')[index] : this;
+
+        //     var name = $(clonedInputs).attr('name') ? $(clonedInputs).attr('name') : '';
+        //     if(name){
+        //         $(clonedInputs).attr('name', 'dm_options['+ id +']['+ repeatCount +']['+ name +']')
+        //     }
+        // });
         // resetting data
         jQuery(window).trigger('dm-scripts.dm', [$('.dm-'+id+'-repeater-control').last()]);
     });
@@ -35,5 +65,6 @@ jQuery(document).ready(function ($) {
     // deleting repeater
     $(document).on('click', '.dm-editor-post-trash',function(){
         $(this).parents('.dm-repeater-control').remove();
+        jQuery('.dm-repeater-add-new').trigger('click', [true]);
     })
 });
