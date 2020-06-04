72ec9853d4e5f8961dc3a14cfcb51071396e1aa2
Reza Khan <rezakhan995@gmail.com>
2020-06-04 17:25:55 +0600

    wp-editor added, need some js fixes

--- core/options/customizer/controls/wp-editor/wp-editor.php
+++ core/options/customizer/controls/wp-editor/wp-editor.php
@@ -7,26 +7,26 @@ if ( !class_exists( 'WP_Customize_Control' ) ) {
 
 class WpEditor extends \WP_Customize_Control {
 
-    public $label, $name, $desc, $value, $settings;
+    public $label, $name, $desc, $value;
 
     /**
-     * The type of customize control being rendered.
-     *
-     * @since  1.0.0
-     * @access public
-     * @var    string
-     */
-    public $type = 'wp-editor';
+	 * The type of customize control being rendered.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var    string
+	 */
+	public $type = 'wp-editor';
 
     public $statuses;
 
     /**
-     * Constructor of this control. Must call parent constructor
-     *
-     * @since  1.0.0
-     * @access public
-     * @var    string
-     */
+	 * Constructor of this control. Must call parent constructor
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var    string
+	 */
     public function __construct( $manager, $id, $args = [] ) {
         $this->prepare_values( $id, $args );
         $this->statuses = ['' => __( 'Default' )];
@@ -41,19 +41,22 @@ class WpEditor extends \WP_Customize_Control {
      * @return void
      */
     private function prepare_values( $id, $args = [] ) {
-        $this->label                     = isset( $args[0]['label'] ) ? $args[0]['label'] : "";
-        $this->name                      = isset( $args[0]['id'] ) ? $args[0]['id'] : "";
-        $this->desc                      = isset( $args[0]['desc'] ) ? $args[0]['desc'] : "";
-        $this->settings["wpautop"]       = ( isset( $args[0]['wpautop'] ) ) ? $args[0]['wpautop'] : false;
-        $this->settings["editor_height"] = ( isset( $args[0]['editor_height'] ) ) ? (int) $args[0]['editor_height'] : 285;
-        $this->settings["tinymce"]       = ( isset( $args[0]['editor_type'] ) && $args[0]['editor_type'] === false ) ? false : true;
+        $this->label         = isset( $args[0]['label'] ) ? $args[0]['label'] : "";
+        $this->name          = isset( $args[0]['id'] ) ? $args[0]['id'] : "";
+        $this->desc          = isset( $args[0]['desc'] ) ? $args[0]['desc'] : "";
     }
 
-    /**
-     * @internal
+    function editor_customizer_script() {
+        wp_enqueue_script( 'wp-editor-customizer', DM_CORE . 'options/customizer/controls/wp-editor/assets/js/script.js', array( 'jquery' ), rand(), true );
+    }
+    
+    /*
+     ** Enqueue control related scripts/styles
      */
     public function enqueue() {
-
+        add_action( 'customize_controls_enqueue_scripts', [$this, 'editor_customizer_script'] );
+         
+        // wp_enqueue_script( 'dm-customizer-wpeditor-handle', DM_CORE . 'options/customizer/controls/wp-editor/assets/js/script.js', ['jquery'], false, true );
     }
 
     /**
@@ -68,27 +71,40 @@ class WpEditor extends \WP_Customize_Control {
      * @internal
      */
     public function render_content() {
-        $wrapper_attr['data-nonce']   = wp_create_nonce( '_action_get_oembed_response' );
-        $wrapper_attr['data-preview'] = $this->data_preview;
-        // var_dump("Hello");
         ?>
             <li class="dm-option">
                 <div class="dm-option-column left">
                     <label class="dm-option-label"><?php echo esc_html( $this->label ); ?> </label>
                 </div>
 
-                <div class="dm-option-column right">
-                    <?php
-                    wp_editor( $this->value(), $this->name, $this->settings );
-                    $editor_html = ob_get_contents();
-                    $editor_html .= "<p class='dm-option-desc'>" . esc_html( $this->desc ) . " </p>";
-                    ob_end_clean();
-
-                    echo dm_render_markup( $editor_html );
-                    ?>
+                <div class="dm-option-column right wp-editor-parent">
+                    
+                    <input class="wp-editor-area" type="hidden" <?php $this->link(); ?> value="<?php echo esc_textarea( $this->value() ); ?>">
+                        <?php
+                        $content = $this->value();
+                        $editor_id = $this->name;
+                        $settings = array(
+                            'textarea_name' => $this->name,
+                            'media_buttons' => true,
+                            'drag_drop_upload' => false,
+                            'teeny' => true,
+                            'quicktags' => true,
+                            'textarea_rows' => 5,
+                        );
+                        $this->filter_editor_setting_link();
+                        wp_editor($this->value(), $this->id, $settings );
+                        
+                        do_action('admin_footer');
+                        do_action('admin_print_footer_scripts');
+                        ?>
+                    <p class="dm-option-desc"><?php echo esc_html( $this->desc ); ?> </p>
                 </div>
             </li>
     <?php
-}
+    }
+
+    private function filter_editor_setting_link() {
+        add_filter( 'the_editor', function( $output ) { return preg_replace( '/<textarea/', '<textarea ' . $this->get_link(), $output, 1 ); } );
+    }
 
 }
