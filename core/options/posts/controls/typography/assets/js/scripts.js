c8bb3b89f6bc2c71786fd39c1f3c00b4d0908732
easin-xs <easin@xpeedstudio.com>
2020-06-07 15:38:54 +0600

    typography done

--- core/options/posts/controls/typography/assets/js/scripts.js
+++ core/options/posts/controls/typography/assets/js/scripts.js
@@ -1,76 +1,50 @@
-jQuery(document).ready(function ($) {
-
-    // for color
-    var dmOptions = {
+jQuery(window).on('dm-scripts-typo.dm', function(e,val){
+    var dmColorOptions = {
+        defaultColor: dm_color_picker_config.default,
+        hide: true,
+        palettes: dm_color_picker_config.palettes
+    };
+    var el = jQuery('.dm-option.active-script .google-fonts-list');
+   // for color
+   var dmOptions = {
         defaultColor: typo_config.selected_data.color,
         hide: true,
     };
-    $('.dm-typography-color-field').wpColorPicker(dmOptions);
-    
+    jQuery('.dm-option.active-script .dm-typography-color-field').wpColorPicker(dmOptions);
+
     // for select2 
-    $('.google-fonts-list').select2();
+    jQuery('.dm-option.active-script .google-fonts-list').select2();
 
     // select 2 on change style and weight
-    $('.google-fonts-list').on("change", function (e) {
-        // console.log("font changed");
-        var selected_value = $(".google-fonts-list option:selected").text();
-        $('.google-weight-list').html(" ");
-        $('.google-style-list').html(" ");
-
+    jQuery('.dm-option.active-script .google-fonts-list').on("change", function (e) {
+        var self = jQuery(this),
+            parent = self.parents('.dm-option-typography'),
+            selected_value = self.val();
+            console.log(typo_config.font_list);
         if (typo_config.font_list.length > 0) {
-            $.each(typo_config.font_list, function (key, item) {
+            jQuery.each(typo_config.font_list, function (key, item) {
                 if (item.family == selected_value) {
                     // weight
-                    $.each(item.variants, function (i, variant) {
-                        $('.google-weight-list').append(
+                    jQuery.each(item.variants, function (i, variant) {
+                        parent.find('.google-weight-list').append(
                             '<option value=' + variant + ' >' + variant + '</option>'
                         );
                     });
                     // style
-                    $.each(item.subsets, function (i, style) {
-                        $('.google-style-list').append(
+                    jQuery.each(item.subsets, function (i, style) {
+                        parent.find('.google-style-list').append(
                             '<option value=' + style + ' >' + style + '</option>'
                         )
                     });
-
+                    return false
                 }
             })
         }
     });
 
-    
-    // if value selected
-    if (typo_config.selected_data.style !== '') {
-        $('.google-weight-list').html(" ");
-        $('.google-style-list').html(" ");
-        $.each(typo_config.font_list, function (key, item) {
-            if (item.family == typo_config.selected_data.family) {
-                // weight
-                $.each(item.variants, function (i, variant) {
-                    var selected_weight = variant == typo_config.selected_data.weight ? 'selected' : '';
-                    $('.google-weight-list').append(
-                        '<option value=' + variant + ' ' + selected_weight + '>' + variant + '</option>'
-                    );
-                    // style
-                    $.each(item.subsets, function (i, style) {
-                        var selected_style = style == typo_config.selected_data.style ? 'selected' : '';
-                        $('.google-style-list').append(
-                            '<option value=' + style + ' ' + selected_style + '>' + style + '</option>'
-                        )
-                    });
-                });
-            }
-        })
-    }
+    jQuery('.dm-option.active-script .google-fonts-list').trigger('change');
+});
 
-    // slide ranger
-    $(document).on('change', '.range-slider-font-size', function () {
-        document.getElementById('size_value').value = $(this).val();
-    });
-    $(document).on('change', '.range-slider-line-height', function () {
-        document.getElementById('line_height_value').value = $(this).val();
-    });
-    $(document).on('change', '.range-slide-letter-space', function () {
-        document.getElementById('latter_spacing_value').value = $(this).val();
-    });
+jQuery(document).ready(function($){
+    jQuery(window).trigger('dm-scripts-typo.dm');
 });
