4ae0155bbafb3719087eba8d9fae3f62acbecb6a
Reza Khan <rezakhan995@gmail.com>
2020-06-10 11:20:16 +0600

    customizer image-picker: css is now used from post control, so duplicated css file is not needed anymore

--- core/options/customizer/controls/image-picker/image-picker.php
+++ core/options/customizer/controls/image-picker/image-picker.php
@@ -43,12 +43,8 @@ class ImagePicker extends Structure {
      ** Enqueue control related scripts/styles
      */
     public function enqueue() {
-        // js
-        wp_enqueue_script( 'dm-image-picker-js', plugins_url( 'image-picker/assets/js/image-picker.js', dirname( __FILE__ ) ), ['jquery'], time(), true );
         // css
-        wp_enqueue_style( 'dm-image-picker-css', plugins_url( 'image-picker/assets/css/image-picker.css', dirname( __FILE__ ) ) );
-        // var_dump($this->name);
-        wp_localize_script( 'dm-image-picker-js', 'settings_id', $this->name );
+        wp_enqueue_style( 'dm-image-picker-css', DM_CORE . 'options/posts/controls/image-picker/assets/css/image-picker.css' );
     }
 
     
