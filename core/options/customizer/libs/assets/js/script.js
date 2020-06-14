525b88e07a15240cec641053e944330b571f2c35
easin-xs <easin@xpeedstudio.com>
2020-06-14 17:57:04 +0600

    fixed customizer issues

--- core/options/customizer/libs/assets/js/script.js
+++ core/options/customizer/libs/assets/js/script.js
@@ -1,3 +1,24 @@
-jQuery(document).ready(function($){
+jQuery(window).on('load',function($){
     console.log("global");
+
+    jQuery('input.dm-ctrl.dm-control-switcher').each(function(){
+        var name = jQuery(this).attr('data-customize-setting-link'),   
+            value = jQuery(this).attr('data-value');
+
+            var self = jQuery(this);
+        wp.customize( name, function ( obj ) {
+            if(self.hasClass('dm-control-switcher')){
+                var rightKey = self.data('right_key');
+                if(rightKey == value){
+                    value = 1;
+                } else {
+                    value = 0;
+                }
+            }
+            obj.set( value );
+            self.parents('.dm-option').addClass('active-script');
+            jQuery(window).trigger('dm-scripts.dm');
+        } );
+    });
+    
 });
--- core/options/posts/controls/datetime-picker/assets/js/script.js
+++ core/options/posts/controls/datetime-picker/assets/js/script.js
@@ -1,6 +1,6 @@
 jQuery(window).on('dm-scripts.dm', function(){
     var el = jQuery('.dm-option.active-script .dm-option-input-datetime-picker');
-    console.log(date_time_picker_config);
+
     //Initialize the datepicker and set the first day of the week as Monday
     if (el.length && !el.hasClass('flatpickr-input')) {
         var time_picker = (date_time_picker_config.timepicker == 0) ? false : true;
