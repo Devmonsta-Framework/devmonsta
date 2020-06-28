4982bd08f0b3749ab5a5b9fd9ddc0b40b4062c90
easin-xs <easin@xpeedstudio.com>
2020-06-28 17:35:38 +0600

    removed console.log

--- core/options/assets/src/init-scripts/select.js
+++ core/options/assets/src/init-scripts/select.js
@@ -1,9 +1,6 @@
 jQuery(window).on('dm-scripts.select', function(e, output){
     var el = jQuery('.dm-option.active-script .dm_select');
-    console.log(el);
-    // if(output){
-    //     el = output.find('.dm_select')
-    // }
+
     el.select2();
 });
 
