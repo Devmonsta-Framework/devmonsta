43fc9d401f026c4c005f7c4bdb5651f0f71e040b
easin-xs <easin@xpeedstudio.com>
2020-06-29 16:23:28 +0600

    removed assets from post controls

--- core/options/posts/controls/select/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-jQuery(window).on('dm-scripts.select', function(){
-    var el = jQuery('.dm-option.active-script .dm_select');
-    el.select2();
-});
-
-jQuery(document).ready(function($) {
-    console.log('ok done')
-    jQuery(window).trigger('dm-scripts.select');
-});
