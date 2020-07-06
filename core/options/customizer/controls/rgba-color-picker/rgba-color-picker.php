22a57229fd4fcff1b1a3a778bdfcc04c6b6b6ed3
Reza Khan <rezakhan995@gmail.com>
2020-07-06 18:11:19 +0600

    customizer rgba-color-picker control changed, solved conflict with default wordpress themes

--- core/options/customizer/controls/rgba-color-picker/rgba-color-picker.php
+++ core/options/customizer/controls/rgba-color-picker/rgba-color-picker.php
@@ -28,7 +28,7 @@ class RgbaColorPicker extends Structure {
      */
     public function __construct( $manager, $id, $args = [] ) {
         $this->prepare_values( $id, $args );
-        $this->statuses = ['' =>esc_html__( 'Default' )];
+        $this->statuses = ['' => esc_html__( 'Default' )];
         parent::__construct( $manager, $id, $args );
     }
 
@@ -43,8 +43,8 @@ class RgbaColorPicker extends Structure {
         $this->label         = isset( $args[0]['label'] ) ? $args[0]['label'] : "";
         $this->name          = isset( $args[0]['id'] ) ? $args[0]['id'] : "";
         $this->desc          = isset( $args[0]['desc'] ) ? $args[0]['desc'] : "";
-        $this->default_value = isset( $args[0]['value'] ) && preg_match('/rgba\((\s*\d+\s*,){3}[\d\.]+\)/', $args[0]['value']) ? $args[0]['value'] : "";
-        $this->palettes      = isset( $args[0]['palettes'] ) && is_array( $args[0]['palettes'] )? $args[0]['palettes'] : [];
+        $this->default_value = isset( $args[0]['value'] ) && preg_match( '/rgba\((\s*\d+\s*,){3}[\d\.]+\)/', $args[0]['value'] ) ? $args[0]['value'] : "";
+        $this->palettes      = isset( $args[0]['palettes'] ) && is_array( $args[0]['palettes'] ) ? implode( '|', $args[0]['palettes'] ) : [];
 
         //generate attributes dynamically for parent tag
         $this->default_attributes = $this->prepare_default_attributes( $args[0], "active-script" );
@@ -55,16 +55,19 @@ class RgbaColorPicker extends Structure {
      */
     public function enqueue() {
 
-        if ( !wp_style_is( 'wp-color-picker', 'enqueued' ) ) {
-            wp_enqueue_style( 'wp-color-picker' );
-        }
-        wp_enqueue_script( 'devm-rgba-handle-from-post', DEVMONSTA_CORE . 'options/posts/controls/rgba-color-picker/assets/js/wp-color-picker-alpha.js', ['jquery', 'wp-color-picker'], false, true );
-        wp_enqueue_script( 'devm-customizer-rgba-handle', DEVMONSTA_CORE . 'options/customizer/controls/rgba-color-picker/assets/js/wp-color-picker-alpha.js', ['jquery', 'wp-color-picker', 'devm-rgba-handle-from-post'], false, true );
+        wp_enqueue_script( 'alpha-color-picker', DEVMONSTA_CORE . 'options/customizer/controls/rgba-color-picker/assets/js/alpha-color-picker.js', [ 'jquery', 'wp-color-picker' ], false, true );
+        wp_enqueue_style( 'alpha-color-picker', DEVMONSTA_CORE . 'options/customizer/controls/rgba-color-picker/assets/css/alpha-color-picker.css', [ 'wp-color-picker' ] );
 
-        $data             = [];
-        $data['default']  = $this->default_value;
-        $data['palettes'] = $this->palettes;
-        wp_localize_script( 'devm-customizer-rgba-handle', 'rgba_color_picker_config', $data );
+        // if ( !wp_style_is( 'wp-color-picker', 'enqueued' ) ) {
+        //     wp_enqueue_style( 'wp-color-picker' );
+        // }
+        // wp_enqueue_script( 'devm-rgba-handle-from-post', DEVMONSTA_CORE . 'options/posts/controls/rgba-color-picker/assets/js/wp-color-picker-alpha.js', ['jquery', 'wp-color-picker'], false, true );
+        // wp_enqueue_script( 'devm-customizer-rgba-handle', DEVMONSTA_CORE . 'options/customizer/controls/rgba-color-picker/assets/js/wp-color-picker-alpha.js', ['jquery', 'wp-color-picker', 'devm-rgba-handle-from-post'], false, true );
+
+        // $data             = [];
+        // $data['default']  = $this->default_value;
+        // $data['palettes'] = $this->palettes;
+        // wp_localize_script( 'devm-customizer-rgba-handle', 'rgba_color_picker_config', $data );
     }
 
     /**
@@ -74,7 +77,6 @@ class RgbaColorPicker extends Structure {
         $this->value = ( !is_null( $this->value() ) && !empty( $this->value() ) ) ? $this->value() : $this->default_value;
         $this->render_content();
     }
-
 
     /**
      * Generates markup for specific control
@@ -88,17 +90,11 @@ class RgbaColorPicker extends Structure {
                 </div>
 
                 <div class="devm-option-column right">
-                    <input type="text" <?php $this->link(); ?>
-                        name="<?php echo esc_attr( $this->name ); ?>"
-                        value="<?php echo esc_attr( $this->value ); ?>"
-                        class="devm-ctrl devm-color-field color-picker-rgb"
-                        data-alpha="true"
-                        data-default-color="<?php echo esc_attr( $this->value ); ?>" />
-                    
+                    <input class="alpha-color-control" type="text" data-show-opacity="true" data-palette="<?php echo esc_attr( $this->palettes ); ?>" data-default-color="<?php echo esc_attr( $this->value ); ?>" <?php $this->link();?>  />
                     <p class="devm-option-desc"><?php echo esc_html( $this->desc ); ?> </p>
                 </div>
             </li>
         <?php
-    }
+}
 
 }
