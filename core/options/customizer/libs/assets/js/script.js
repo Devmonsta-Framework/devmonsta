3118f1912f6a39e048abc54218accb3ef06fe940
Md Easin <easin@xpeedstudio.com>
2020-11-11 18:50:27 +0600

    newly designed customizer switcher

--- core/options/customizer/libs/assets/js/script.js
+++ core/options/customizer/libs/assets/js/script.js
@@ -1,19 +1,11 @@
 jQuery(window).on('load',function($){
 
     jQuery('.devm-ctrl').each(function(){
-        var name = jQuery(this).attr('data-customize-setting-link'),   
+        var self = jQuery(this),
+            name = jQuery(this).attr('data-customize-setting-link'),   
             value = jQuery(this).data('value');
 
-            var self = jQuery(this);
         wp.customize( name, function ( obj ) {
-            if(self.hasClass('devm-control-switcher')){
-                var rightKey = self.data('right_key');
-                if(rightKey == value){
-                    value = 1;
-                } else {
-                    value = 0;
-                }
-            }
             obj.set( value );
             self.parents('.devm-option').addClass('active-script');
             jQuery(window).trigger('devm-scripts.dm');
