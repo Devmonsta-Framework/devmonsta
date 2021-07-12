338b5ed656b120131337b8b2cead450070a61548
mohon-xs <mohon@xpeedstudio.com>
2021-07-12 17:02:11 +0600

    tweak: Gallery support for Upload Control on Post and Taxonomy.

--- core/options/assets/src/init-scripts/upload.js
+++ core/options/assets/src/init-scripts/upload.js
@@ -1,39 +1,77 @@
-jQuery(document).ready(function($) {
+(function ($) {
+    'use strict';
 
-    $('body').on('click', '.devm_upload_image_button', function(e) {
+    $(function () {
+        $('body').on('click', '.devm-option-upload--child', function () {
+            var $el = $(this),
+                $parent = $el.parents('.devm-option-upload--list'),
+                $input = $parent.next('input'),
+                isMulti = $parent.data('multiple') ? 'add' : false,
+                uploader;
 
-        e.preventDefault();
-        var multiple = false,
-            self = $(this);
-        if ($(this).data('multiple')) {
-            multiple = Boolean($(this).data('multiple'));
-        }
-
-        var button = $(this),
-            custom_uploader = wp.media({
-                title: 'Insert image',
+            // Register: Media Library
+            uploader = wp.media({
+                title: 'Insert Image',
                 library: {
                     type: 'image'
                 },
-                button: {
-                    text: 'Use this image' 
-                },
-                multiple: multiple
-            }).on('select', function() {
-                var attachment = custom_uploader.state().get('selection').first().toJSON();
-                $(button).removeClass('button').html('<img class="true_pre_image" src="' + attachment.url + '" style="max-width:95%;display:block;" />').next().val(attachment.id).next().show();
-                self.parent().find('.devm-upload').trigger('input');
+                multiple: isMulti
+            }).on('open', function () {
+                var selection = uploader.state().get('selection'),
+                    img_ids = $input.val();
+                
+                if ( img_ids.length ) {
+                    img_ids = img_ids.split(',');
 
-            })
-            .open();
-    });
+                    img_ids.forEach(function (id) {
+                        selection.add( wp.media.attachment(id) );
+                    });
+                }
+            }).on('select', function () {
+                var imgList = uploader.state().get('selection').toJSON();
 
-    /*
-     * Remove image
-     */
-    $('body').on('click', '.devm_remove_image_button', function() {
-        $(this).hide().prev().val('').prev().addClass('button').html('Upload image');
-        $(this).parent().find('.devm-upload').trigger('input');
-        return false;
+                $parent.toggleClass('is--multiple', (imgList.length > 1)).empty();
+
+                $.each(imgList, function (indx, img) {
+                    $parent.append(`<div class="devm-option-upload--item">
+                        <img src="${img.url}" class="devm-option-upload--child">
+                        <button type="button" class="devm-option-upload--remove dashicons dashicons-dismiss" data-id="${img.id}"></button>
+                    </div>`);
+                });
+
+                $input.val( imgList.map(val => val.id) ).trigger('input');
+            });
+
+            // Open: Media Library
+            uploader.open();
+        }).on('click', '.devm-option-upload--remove', function () {
+            var $el = $(this),
+                $parent = $el.parents('.devm-option-upload--list'),
+                $input = $parent.next('input'),
+                imgID = String( $el.data('id') );
+
+            // update: values
+            $input.val(function (i, el) {
+                var ids = el.split(','),
+                    indx = ids.indexOf( imgID );
+
+                if ( indx !== -1 ) {
+                    ids.splice(indx, 1);
+                }
+
+                return ids.join(',');
+            }).trigger('input');
+
+            // visual: remove image
+            $el.parent().remove();
+
+            // state: is--multiple
+            $parent.toggleClass('is--multiple', ($parent[0].childElementCount > 1));
+
+            // add: button
+            if ( $parent[0].childElementCount === 0 ) {
+                $parent.html('<div class="devm-option-upload--item"><button type="button" class="devm-option-upload--child button">Upload Image</button></div>');
+            }
+        });
     });
-});
+}(jQuery));
